865f108d1a101b3e5180b117ce348936eaf9489e
davidmrt98 <62178322+davidmrt98@users.noreply.github.com>
2020-05-09 20:40:43 +0200

    Delete common.py

--- src/visuanalytics/app/apis/common.py
+++
@@ -1,8 +0,0 @@
-import json
-from pip._vendor import requests
-
-def get_api_data(url):
-    response = requests.get(url)
-    if(response.status_code != 200):
-        raise ValueError
-    return json.load(response.content)
