7be0bb62e7f9a6dfe055cd9a70d966ada02fbdb3
Jannik l <jannik.lapp@mni.thm.de>
2020-05-10 10:02:27 +0200

    methode hinzugefügt die das Video erstellt

--- src/visuanalytics/app/visualization/WeatherVisualization.py
+++ src/visuanalytics/app/visualization/WeatherVisualization.py
@@ -1,5 +1,6 @@
 """Klasse dient dazu um aus gegebenen Daten von der Weather API Bilder oder Videos zu generieren.
 """
+import os
 
 from PIL import Image
 from PIL import ImageDraw
@@ -16,6 +17,50 @@ LOCATIONS_TEMP_MIN_DREITAGE = [(160, 950), (790, 950), (1400, 950)]
 LOCATIONS_TEMP_MAX_DREITAGE = [(450, 950), (1070, 950), (1700, 950)]
 """Liste aus Tupeln: X und Y Koordinaten der Max Temperaturen in der 3 Tages Vorhersage.
 """
+
+
+def generate_eins_drei_video(bild_heute_art, bild_heute_temp, bild_dreitage, audio_heute_art,
+                             audio_heute_temp, audio_dreitage, audiol_heute_art, audiol_heute_temp,
+                             audio3_dreitage):
+    """Methode wird genutzt um das gesamte Video (1+3) zu erstellen
+            Args:
+                bild_heute_art (String): Dateinamen des Bildes für die Vorhersage für morgen (Icons)
+                bild_heute_temp (String): Dateinamen des Bildes für die Vorhersage für morgen (Temperatur)
+                bild_dreitage (String): Dateinamen des Bildes für die Vorhersage für die nächsten 3 Tage
+                audio_heute_art (String): Dateinamen des Audios für die Vorhersage für morgen (Icons)
+                audio_heute_temp (String): Dateinamen des Audios für die Vorhersage für morgen (Temperatur)
+                audio_dreitage (String): Dateinamen des Audios für die Vorhersage für die nächsten 3 Tage
+                audiol_heute_art (Int): Audiolänge des Audios für die Vorhersage für morgen (Icons)
+                audiol_heute_temp (Int): Audiolänge des Audios für die Vorhersage für morgen (Temperatur)
+                audio3_dreitage (Int): Audiolänge des Audios für die Vorhersage für die nächsten 3 Tage
+           Returns:
+              String : Den Dateinamen des erstellten Bildes
+       """
+
+    file = open("input.txt", "w")
+    file.write("file '" + audio_heute_art + "'\n")
+    file.write("file '" + audio_heute_temp + "'\n")
+    file.write("file '" + audio_dreitage + "'\n")
+    file.close()
+
+    shell_cmd = "ffmpeg -f concat -i input.txt -c copy output.wav"
+    os.chdir("/home/jannik/Data-Analytics/src/visuanalytics/app")
+    os.system(shell_cmd)
+
+    file = open("input.txt", "w")
+    file.write("file '" + bild_heute_art + "'\n")
+    file.write("duration " + audiol_heute_art + "\n")
+    file.write("file '" + bild_heute_temp + "'\n")
+    file.write("duration " + audiol_heute_temp + "\n")
+    file.write("file '" + bild_dreitage + "'\n")
+    file.write("duration " + audio3_dreitage + "\n")
+    file.close()
+
+    shell_cmd = "ffmpeg -y -f concat -i input.txt -i output.wav -s 1920x1080 output.mp4"
+    os.chdir("/home/jannik/Data-Analytics/src/visuanalytics/app")
+    os.system(shell_cmd)
+
+    return "output.mp4"
 
 
 def temp_method_toget_image(location, date_in_future):
