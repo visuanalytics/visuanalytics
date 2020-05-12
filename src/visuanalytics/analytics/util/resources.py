55cf7e9c2fb46f3e2a81b3fc7861b3dd959e349b
Max Stephan <max.stephan@mni.thm.de>
2020-05-12 21:39:44 +0200

    Resorces utils hinzugefügt

---
+++ src/visuanalytics/analytics/util/resources.py
@@ -0,0 +1,55 @@
+"""Module, dass Funktionen zur Benutzung von Ressourcen bereitstellt."""
+import contextlib
+import os
+
+RESOURCES_LOCATION = "../../resources"
+"""
+Relativer pfad zu dem resources Ordner.
+"""
+
+
+# TODO(Max) vtl. für tmp resources Funktionen erstellen die,
+#  die resourcen mit id generierten Ordnernamen erstellt / bekommt.
+
+def get_resource_path(path: str):
+    """Erstellt einen Absoluten Pfad zu der übergebene Ressource.
+
+    Erstellt den Pfad aus `RESOURCES_LOCATION` und dem übergebenen Pfad.
+
+    :param path: Pfad zur Ressource, relativ zum `resources` Ordner.
+    :return: Absoluter Pfad zur übergebener Ressource.
+    """
+    return os.path.abspath(os.path.join(RESOURCES_LOCATION, path))
+
+
+def open_resource(path: str, mode: str = "rt"):
+    """Öffnet die übergebene Ressource.
+
+    Verwendet :func:`get_resource_path` um den Pfad der Ressource zu bekommen.
+    Ist die Datei oder darüber liegende Ordner nicht vorhanden werden diese erstellt.
+
+    :param path: Pfad zur Resource, Relativ zum `resources` Ordner.
+    :param mode: Mode zum Öffnen der Datei siehe :func:`open`.
+
+    :return: Die geöffnete Datei (siehe :func:`open`)
+
+    :raises: OSError
+    """
+    res_path = get_resource_path(path)
+    os.makedirs(os.path.dirname(res_path), exist_ok=True)
+
+    return open(res_path, mode)
+
+
+def delete_resource(path: str):
+    """Löscht die übergebene Ressource.
+
+    Verwendet :func:`get_resource_path` um den Pfad der Ressource zu bekommen.
+    Ist die Ressource nicht vorhanden wird das ignoriert.
+
+    :param path: Pfad zur Ressource, relativ zum `resources` Ordner.
+
+    :raises: OSError
+    """
+    with contextlib.suppress(FileNotFoundError):
+        os.remove(get_resource_path(path))
