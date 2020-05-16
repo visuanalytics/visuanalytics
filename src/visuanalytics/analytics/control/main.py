d8c8d87cf5c58d7e1300f1e0913e5e15bfc184bf
Max Stephan <max.stephan@mni.thm.de>
2020-05-16 13:05:00 +0200

    conrol main: changed id -> uuid

--- src/visuanalytics/analytics/control/main.py
+++ src/visuanalytics/analytics/control/main.py
@@ -1,6 +1,8 @@
+import uuid
+
 from visuanalytics.analytics.control.jobs.job import Job
 from visuanalytics.analytics.control.steps.weather import WeatherSteps
 
 # TODO(Max) Implement (current just for testing)
 
-Job("1", WeatherSteps("Config")).start()
+Job(uuid.uuid4().hex, WeatherSteps({})).start()
