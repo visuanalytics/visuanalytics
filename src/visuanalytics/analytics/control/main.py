486bfe95deecffd0d71de79eb644b3f4b77a7805
Max Stephan <max.stephan@mni.thm.de>
2020-05-18 19:09:22 +0200

    Output ordner geändert

--- src/visuanalytics/analytics/control/main.py
+++ src/visuanalytics/analytics/control/main.py
@@ -1,12 +1,17 @@
+import os
 import uuid
 
 from visuanalytics.analytics.control.pipeline import Pipeline
 from visuanalytics.analytics.control.procedures.weather import WeatherSteps
-
-
 # TODO(Max) Implement (current just for testing)
+from visuanalytics.analytics.util import resources
+
 
 def main():
+    # Not ready will be moved later
+    os.makedirs(resources.get_resource_path("temp"), exist_ok=True)
+    os.makedirs(resources.get_resource_path("out"), exist_ok=True)
+
     Pipeline(uuid.uuid4().hex, WeatherSteps({"testing": True})).start()
 
 
