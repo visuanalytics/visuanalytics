44fb36e6c312835f4370fb3cc62be68b655400e7
Max Stephan <max.stephan@mni.thm.de>
2020-05-18 16:53:57 +0200

    Test weather visualization: import fehler behoben

--- src/visuanalytics/tests/analytics/processing/weather/test_weather_visualization.py
+++ src/visuanalytics/tests/analytics/processing/weather/test_weather_visualization.py
@@ -1,39 +1,39 @@
+import json
 import os
+import unittest
 
-from visuanalytics.analytics.preprocessing.weather import visualisation
+from visuanalytics.analytics.preprocessing.weather import visualisation as pre_visualisation
+from visuanalytics.analytics.processing.weather import visualisation as pro_visualisation
 from visuanalytics.analytics.util import date_time
 from visuanalytics.analytics.util import resources
-from visuanalytics.analytics.processing.weather import visualisation
-import unittest
-import json
 
 
 class PreprocessTest(unittest.TestCase):
     with resources.open_resource("exampledata/example_weather.json") as file_handle:
         input = json.loads(file_handle.read())
-        output = visualisation.preprocess_weather_data(input)
-        date = date_time.date_to_weekday(visualisation.get_first_day(output))
+        output = pre_visualisation.preprocess_weather_data(input)
+        date = date_time.date_to_weekday(pre_visualisation.get_first_day(output))
         path = resources.get_resource_path("temp/weather")
         if not os.path.exists(path):
             os.mkdir(path)
         cleanup = []
 
     def test_if_get_threeday_image_generates_image(self):
-        expected = visualisation.get_threeday_image(visualisation.data_icon_threeday(self.output),
-                                                    visualisation.data_mm_temp_threeday(self.output),
-                                                            self.date[2:5])
+        expected = pro_visualisation.get_threeday_image(pre_visualisation.data_icon_threeday(self.output),
+                                                        pre_visualisation.data_mm_temp_threeday(self.output),
+                                                        self.date[2:5])
         self.cleanup.append(expected)
         assert os.path.exists(resources.get_resource_path(expected)) == 1
 
     def test_if_get_oneday_icons_image_generates_image(self):
-        expected = visualisation.get_oneday_icons_image(visualisation.data_icon_oneday(self.output, 0),
-                                                        self.date[0])
+        expected = pro_visualisation.get_oneday_icons_image(pre_visualisation.data_icon_oneday(self.output, 0),
+                                                            self.date[0])
         self.cleanup.append(expected)
         assert os.path.exists(resources.get_resource_path(expected)) == 1
 
     def test_if_get_oneday_temp_image_generates_image(self):
-        expected = visualisation.get_oneday_temp_image(visualisation.data_temp_oneday(self.output, 0),
-                                                       self.date[0])
+        expected = pro_visualisation.get_oneday_temp_image(pre_visualisation.data_temp_oneday(self.output, 0),
+                                                           self.date[0])
         self.cleanup.append(expected)
         assert os.path.exists(resources.get_resource_path(expected)) == 1
 
