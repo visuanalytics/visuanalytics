ba41c345ba3da6e5a0d97987db3eb6c1b12751da
Max Stephan <max.stephan@mni.thm.de>
2020-05-22 14:44:09 +0200

    Server: Datenbank hinzugefügt

---
+++ src/visuanalytics/server/db/db.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+
+from flask import current_app
+
+
+def connect():
+    con = sqlite3.connect(
+        current_app.config['DATABASE'],
+        detect_types=sqlite3.PARSE_DECLTYPES,
+    )
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def init_db(app):
+    # if db file not exsists create one
+    if not os.path.exists(app.config["DATABASE"]):
+        print("init DB")
+
+        # create dir
+        os.makedirs(os.path.dirname(app.config), exist_ok=True)
+
+        # create database
+        db = sqlite3.connect(
+            app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        with app.open_resource('db/schema.sql') as f:
+            db.executescript(f.read().decode('utf8'))
+
+        db.close()
--- src/visuanalytics/server/server.py
+++ src/visuanalytics/server/server.py
@@ -2,9 +2,12 @@
 
 Enthält die Startkonfiguration für den Flask-Server.
 """
+import os
+
 from flask import Flask
 
 from visuanalytics.server.api import api
+from visuanalytics.server.db import db
 from visuanalytics.server.home import home_views
 
 
@@ -18,14 +21,22 @@ def create_app():
     :rtype: Flask
     """
 
-    # create and configure the app
+    # create
     app = Flask(__name__, instance_relative_config=True)
+
+    # set the right instance folder
+    app.instance_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../instance"))
+
+    # configure the app
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'visuanalytics.db')
     )
 
     # load the instance config, if it exists
     app.config.from_pyfile('config.py', silent=True)
+
+    db.init_db(app)
 
     # Register the Blueprints
     api.register_all(app)
