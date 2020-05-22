5f34f93cfb277dc51d7c43673a4628bc670b7c33
Max Stephan <max.stephan@mni.thm.de>
2020-05-22 18:33:48 +0200

    Server: Funktionen für die datenbank abfrage hinzugefügt

---
+++ src/visuanalytics/server/db/job.py
@@ -0,0 +1,63 @@
+from datetime import time, date
+
+from visuanalytics.server.db import db
+
+
+def create_job(steps_id: int):
+    """Erstelt einen Job in der Datenbank
+
+    :param steps_id: id der vom job auszuführenden Schritte.
+    :type steps_id: int
+    """
+    with db.connect() as con:
+        con.execute("insert into job(steps) values (?)", steps_id)
+        con.commit()
+
+
+def create_schedule(job_id: int, exec_time: time, exec_date: date = None, weekday: int = None, daily: bool = None):
+    """Erstellt einen zeitplan für einen job.
+
+    Es können mehrere zeitpläne für einen job exsistieren, diese werden dann alle unabhänig voneinander ausgeführt.
+    Die zeit muss immer angegeben werden, die werte date und weekday, und daily schließen sich gegenseitig aus, einer muss allerdings vorhanden sein.
+
+    :param job_id: id es zugehörigen jobs.
+    :param exec_time: zeit andem der Job ausgeführt werden soll.
+    :param exec_date: Datum an dem der job ausgeführt werden soll. Wird ein Datum angegeben wird der job nur einmalig ausgeführt. (optional)
+    :param weekday: Wochentag an dem der job ausgeführt werden soll. (optional)
+    :param daily: wenn True wird der job jeden tag ausgeführt. (optional)
+    """
+    # TODO(max) check if just on from date, weekday or daily
+
+    with db.connect() as con:
+        con.execute("insert into schedule(job_id, date, time, weekday, daily) values (?, ?, ?, ?, ?)", job_id,
+                    exec_date, exec_time, weekday, daily)
+        con.commit()
+
+
+def create_steps():
+    """Erstellt eine abfolge von schritten."""
+    # TODO(max)
+    pass
+
+
+def get_schedule(job_id: int):
+    """Gibt alle Zeitpläne für einen job zurück.
+    :param job_id: id des Jobs.
+    :type job_id: int.
+    :return: alle Zeitpläne
+    :rtype: row
+    """
+    with db.connect() as con:
+        return con.execute("select date, time, weekday, daily from schedule where job_id == ?", job_id).fetchall()
+
+
+def get_steps_id(job_id: int):
+    """gibt schritte für einen job zurück.
+
+    :param job_id: id des Jobs.
+    :type job_id: int.
+    :return: die id der zum Job gehörigen Schritten.
+    :rtype: row
+    """
+    with db.connect() as con:
+        return con.execute("select steps from job where id = ?", job_id)
