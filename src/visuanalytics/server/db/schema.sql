83c036c81b8fb58918c1ff6bcca82b026ef9f07c
Max Stephan <max.stephan@mni.thm.de>
2020-05-24 08:51:25 +0200

    DB: config Tabelle hinzugefügt

--- src/visuanalytics/server/db/schema.sql
+++ src/visuanalytics/server/db/schema.sql
@@ -9,16 +9,32 @@ DROP TABLE IF EXISTS steps;
 CREATE TABLE job
 (
     id    INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-    steps INTEGER UNIQUE                           NOT NULL
+    steps INTEGER                                  NOT NULL
+);
+
+-- Table: job_schedule
+CREATE TABLE job_schedule
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    job_id      INTEGER REFERENCES job (id)       NOT NULL,
+    schedule_id INTEGER REFERENCES schedule (id)  NOT NULL
+);
+
+-- Table: job_config
+CREATE TABLE job_config
+(
+    id     INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    job_id INTEGER REFERENCES job (id)              NOT NULL,
+    "key"  TEXT                                     NOT NULL,
+    value  TIME                                     NOT NULL
 );
 
 -- Table: schedule
 CREATE TABLE schedule
 (
-    id      INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE               NOT NULL,
-    job_id REFERENCES job (id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    id      INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
     date    DATE,
-    time    TIME                                                   NOT NULL,
+    time    TIME                                     NOT NULL,
     weekday INTEGER(0) CHECK (weekday >= 0 and weekday <= 6),
     daily   BOOLEAN
 );
@@ -30,6 +46,6 @@ CREATE TABLE steps
     name VARCHAR                                  NOT NULL UNIQUE
 );
 
-
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
+
