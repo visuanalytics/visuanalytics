c4920435e64517730f01cc7f5fab41b6bb0a44c9
Max Stephan <max.stephan@mni.thm.de>
2020-05-24 16:44:32 +0200

    Controll main: Scheduler hinzugefügt

--- src/visuanalytics/analytics/control/main.py
+++ src/visuanalytics/analytics/control/main.py
@@ -1,11 +1,9 @@
-import os
-import uuid
 import logging
+import os
 
-from visuanalytics.analytics.control.pipeline import Pipeline
-from visuanalytics.analytics.control.procedures.weather_single import SingleWeatherSteps
-from visuanalytics.analytics.control.procedures.weather import WeatherSteps
+from visuanalytics.analytics.control.schedule import Scheduler
 from visuanalytics.analytics.util import resources
+
 
 # TODO(Max) Implement (current just for testing)
 
@@ -23,8 +21,9 @@ def main():
     os.makedirs(resources.get_resource_path("temp"), exist_ok=True)
     os.makedirs(resources.get_resource_path("out"), exist_ok=True)
 
+    Scheduler().start()
     # Pipeline(uuid.uuid4().hex, WeatherSteps({"testing": testing})).start()
-    Pipeline(uuid.uuid4().hex, SingleWeatherSteps({"testing": testing, "city_name": "Giessen"})).start()
+    # Pipeline(uuid.uuid4().hex, SingleWeatherSteps({"testing": testing, "city_name": "Giessen"})).start()
 
 
 if __name__ == "__main__":
