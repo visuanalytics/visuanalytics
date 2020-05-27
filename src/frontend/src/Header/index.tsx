241e3c10d3c60330bb8d9003d44a970b81c7af0c
Max Stephan <max.stephan@mni.thm.de>
2020-05-27 18:06:46 +0200

    frontend: Header Component hinzugefügt (Style)

---
+++ src/frontend/src/Header/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { Navigation } from "./Navigation";
+import { Container, Box } from "@material-ui/core";
+import { BlueDivider } from "./BlueDivider";
+
+interface Props {
+  headline: string;
+}
+
+export const Header: React.FC<Props> = ({ headline }) => {
+  return (
+    <AppBar position="static" color="transparent">
+      <Box height="3rem" display="flex" alignSelf="center">
+        <img
+          style={{ maxWidth: "100%", maxHeight: "100%" }}
+          src="/images/logo.png"
+          alt="logo"
+        />
+      </Box>
+      <BlueDivider />
+      <Box display="flex" alignSelf="center" m={1}>
+        <Navigation />
+      </Box>
+      <BlueDivider />
+    </AppBar>
+  );
+};
