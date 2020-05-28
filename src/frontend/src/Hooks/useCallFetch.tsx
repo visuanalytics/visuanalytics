f0005f5dff408174773a9e5f2e7001c4d162dd95
Max Stephan <max.stephan@mni.thm.de>
2020-05-28 09:16:24 +0200

    frontend: useCallFetch hinzugefügt

---
+++ src/frontend/src/Hooks/useCallFetch.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useCallback, useRef } from "react";
+
+/**
+ * Funktion um Json daten von einer URL zu bekommen.
+ * Wird das Component entladen befor der Response da ist wird callBack nicht aufgrufen.
+ *
+ * @param url Request URL (siehe fetch)
+ * @param parms Request parms (siehe fetch)
+ * @param calBack Callback Funktion für einen erfolgreichen Request
+ * @param errorHandle Callback Funktion für den Fehler fall
+ */
+export const useCallFetch = (
+  url: RequestInfo,
+  parms?: RequestInit,
+  callBack?: (jsonData: any) => void,
+  errorHandle?: (err: Error) => void
+) => {
+  const isMounted = useRef(true);
+
+  const cb = useCallback(() => {
+    fetch(url, parms)
+      .then((res) => res.json())
+      .then((data) => {
+        if (isMounted && callBack) callBack(data);
+      })
+      .catch((err) => {
+        if (isMounted && errorHandle) errorHandle(err);
+      });
+  }, [url, parms, callBack, errorHandle, isMounted]);
+
+  useEffect(() => {
+    console.log("effect");
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  return cb;
+};
