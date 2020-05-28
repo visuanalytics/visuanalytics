089f1add8eaaae40f8f5b06a02f5699b0583298e
Max Stephan <max.stephan@mni.thm.de>
2020-05-28 16:58:19 +0200

    frontend App: Header hinzugefügt

--- src/frontend/src/App/index.tsx
+++ src/frontend/src/App/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ComponentProvider } from "../ComponentProvider";
 import { Main } from "./Main";
+import { Header } from "../Header";
 
 const App = () => {
   return (
     <div>
       <ComponentProvider>
+        <Header />
         <Main />
       </ComponentProvider>
     </div>
