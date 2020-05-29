95e55544fb46e157b2602b3ddd596fc770cab3f5
Tanja265 <tanja.gutsche@mni.thm.de>
2020-05-29 10:54:02 +0200

    added docstrings in and refactored filepath_mp3.py to mp3.py and text_to_mp3 method

---
+++ src/visuanalytics/analytics/processing/util/mp3.py
@@ -0,0 +1,26 @@
+"""
+Generierung eines Audiodatei im mp3-Format.
+
+Zuvor erstellter bzw. übergebener Text wird in eine Audiodatei im mp3-Format umgewandelt.
+"""
+from gtts import gTTS
+
+from visuanalytics.analytics.util import resources
+
+
+def text_to_mp3(pipeline_id, text):
+    """Der übergebene Text wird in eine Audiodatei im mp3-Format umgewandelt.
+
+    Der übergebene Text wird in eine Audiodatei im mp3-Format umgewandelt und in einem bestimmten Ordner abgespeichert.
+
+    :param pipeline_id: Ordnername der Audiodatei
+    :type pipeline_id: str
+    :param text:
+    :type text: str
+    :return: Dateipfad als String
+    :rtype: str
+    """
+    tts = gTTS(text, lang='de')
+    file_path = resources.new_temp_resource_path(pipeline_id, "mp3")
+    tts.save(file_path)
+    return file_path
