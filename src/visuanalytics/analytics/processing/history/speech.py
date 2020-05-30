dfd8dd0b1bf9404346d52334892689ae8a345867
Tanja265 <tanja.gutsche@mni.thm.de>
2020-05-30 20:47:39 +0200

    Änderungen & Ergänzungen get_all_audios und _generate_audio

--- src/visuanalytics/analytics/processing/history/speech.py
+++ src/visuanalytics/analytics/processing/history/speech.py
@@ -1,2 +1,25 @@
+from visuanalytics.analytics.processing.util import mp3 as fp
+
+
 def get_all_audios(pipeline_id, data, date):
-    pass
+    return [_generate_audio(pipeline_id, data[1]),
+            _generate_audio(pipeline_id, data[2]),
+            _generate_audio(pipeline_id, data[3]),
+            _generate_audio(pipeline_id, data[4])]
+
+
+def _generate_audio(pipeline_id, data):
+    """
+    :param pipeline_id:
+    :param data: [teaser_1, teaser_2, date_string]
+    :param date: wurde in preprocessing.history.transform.get_date erstellt [date[10], historical_year, years_ago]
+    :return:
+    """
+    text = (
+        f"Heute vor {data['years_ago']} war das Jahr {data['year']}. "
+        f"Ein Thema war damals: {data['teaser_1']}. "
+        f"Ein weiteres Thema war damals: {data['teaser_2']}. "
+    )
+    print(text)
+    file_path = fp.text_to_mp3(pipeline_id, text)
+    return file_path
