0eef24fccd05f78fc12539eb8f8729fe6a1f5398
Tanja265 <tanja.gutsche@mni.thm.de>
2020-05-31 20:01:55 +0200

    output abgeändert, dict war an falscher Stelle

--- src/visuanalytics/analytics/preprocessing/history/transform.py
+++ src/visuanalytics/analytics/preprocessing/history/transform.py
@@ -60,7 +60,6 @@ def preprocess_history_data(json_data):
 
     """
     output = ([], [], [])
-    key_dict = {}
     for idx in range(0, len(json_data)):
         data = []
         for i in range(0, len(json_data[idx]["matches"])):
@@ -73,11 +72,12 @@ def preprocess_history_data(json_data):
         for w in sorted(tosort, key=tosort.get, reverse=True):
             data.append(w)
         output[1].append(data)
+        key_dict = {}
         for j in range(0, int(len(json_data[idx]["facets"]["keyword"]) / 2)):
             key_dict.update(
                 {json_data[idx]["facets"]["keyword"][j * 2]: json_data[idx]["facets"]["keyword"][(j * 2) + 1]})
         output[2].append(key_dict)
-    # print(output[2])
+    print(output[2])
     return output
 
 
