611c309ef5b7f7cd1849146abd4ca07eade95989
Max Stephan <max.stephan@mni.thm.de>
2020-06-01 11:26:47 +0200

    Control main: testing & h264_nvenc zu config.json hinzugefügt

--- src/visuanalytics/analytics/control/main.py
+++ src/visuanalytics/analytics/control/main.py
@@ -3,34 +3,32 @@ import os
 
 from visuanalytics.analytics.control.schedule import Scheduler
 from visuanalytics.analytics.util import resources, external_programms, config_manager
+
+
 # TODO(Max) Implement (current just for testing)
-from visuanalytics.server.db import db
-
-testing = True
-h264_nvenc = False
-
-
 def main():
-    init()
+    config = config_manager.get_public()
 
-    # TODO(max) run in other Thread
+    init(config)
+
     Scheduler().start()
 
-    # Pipeline(uuid.uuid4().hex, WeatherSteps({"testing": testing, "h264_nvenc": h264_nvenc})).start()
-    # Pipeline(uuid.uuid4().hex, SingleWeatherSteps({"testing": testing, "city_name": "Giessen", "h264_nvenc": h264_nvenc})).start()
-    # Pipeline(uuid.uuid4().hex, HistorySteps({"testing": testing, "h264_nvenc": h264_nvenc})).start()
+    # Pipeline(uuid.uuid4().hex, WeatherSteps({"testing": config.get("testing", false), "h264_nvenc": config.get("h264_nvenc", false)})).start()
+    # Pipeline(uuid.uuid4().hex, SingleWeatherSteps({"testing": config.get("testing", false), "h264_nvenc": config.get("h264_nvenc", false), "city_name": "Giessen"})).start()
+    # Pipeline(uuid.uuid4().hex, HistorySteps({"testing": config.get("testing", false), "h264_nvenc": config.get("h264_nvenc", false)})).start()
 
 
-def init():
+def init(config: dict):
     # Check if all external Programmes are installed
-    external_programms.all_installed(config_manager.get_public().get("external_programms", []))
+    external_programms.all_installed(config.get("external_programms", []))
 
     # initialize logging
-    level = logging.INFO if testing else logging.WARNING
+    level = logging.INFO if config.get("testing", False) else logging.WARNING
     logging.basicConfig(format='%(module)s %(levelname)s: %(message)s', level=level)
 
     # init db
-    db.init_db()
+    # DB is currently not in use
+    # db.init_db()
 
     # create temp and out directory
     os.makedirs(resources.get_resource_path("temp"), exist_ok=True)
