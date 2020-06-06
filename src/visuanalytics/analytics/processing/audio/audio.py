300579a1222490a9b2e2fe6d858de7376c796c5d
Tanja265 <tanja.gutsche@mni.thm.de>
2020-06-06 17:51:31 +0200

    audio.py sollte Beispiel-json ausführen

--- src/visuanalytics/analytics/processing/audio/audio.py
+++ src/visuanalytics/analytics/processing/audio/audio.py
@@ -1,3 +1,5 @@
+import json
+
 from gtts import gTTS
 
 from visuanalytics.analytics.processing.audio.parts import part
@@ -10,3 +12,10 @@ def generate_audio(name, value, config):
     file_path = resources.new_temp_resource_path(name, config["format"])
     tts.save(file_path)
     return file_path
+
+
+with open("/exampledata/step-example_weather_single_test.json") as fp:
+    d = json.loads(fp)
+
+x = generate_audio(d["name"], d["audio"]["audios"]["a1"], d["audio"]["config"])
+print(x)
