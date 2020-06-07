e18acdb7aba8bedb3712f8101ca9d29d78a722ab
David Martschenko <david.martschenko@web.de>
2020-06-07 19:09:32 +0200

    Zeitplan-Auswahl-Komponente fertiggestellt

--- src/frontend/src/JobCreate/ScheduleSelection/index.tsx
+++ src/frontend/src/JobCreate/ScheduleSelection/index.tsx
@@ -1,106 +1,65 @@
 import React from "react";
 import { useStyles } from "../style";
-import { Divider, Checkbox, FormControlLabel, FormControl, FormLabel, FormGroup, FormHelperText } from "@material-ui/core";
-import { KeyboardDatePicker, MuiPickersUtilsProvider, KeyboardTimePicker } from "@material-ui/pickers";
+import { Divider, FormControlLabel, Radio } from "@material-ui/core";
 import { ContinueButton } from "../ContinueButton";
-import DateFnsUtils from '@date-io/date-fns';
-import { de } from "date-fns/locale";
+import { GreyDivider } from "../GreyDivider";
+import { WeekdayCheckboxes } from "./WeekdayCheckboxes";
+import { DateInputField, TimeInputField } from "./DateTimeInput"
 
 export const ScheduleSelection: React.FC = () => {
     const classes = useStyles();
-    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
-        new Date(),
-    );
+
+    const [selectedDate, setSelectedDate] = React.useState<Date | null>(new Date());
+    const [selectedSchedule, setSelectedSchedule] = React.useState("daily");
+
     const handleDateChange = (date: Date | null) => {
         setSelectedDate(date);
     }
+    const handleScheduleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedSchedule(event.target.value);
+    };
+
     return (
         <div className={classes.jobCreateBox}>
             <div>
                 <h3 className={classes.jobCreateHeader}>Zeitplan festlegen</h3>
                 <p>Wann sollen neue Videos generiert werden?</p>
             </div>
-            <Divider />
-            <div className={classes.centerBox}>
-                <FormControl component="fieldset">
-                    <FormGroup>
-                        <FormControlLabel
-                            control={<Checkbox name="gilad" />}
-                            label="täglich" />
-                    </FormGroup>
-                </FormControl>
+            <GreyDivider />
+            <div className={classes.paddingSmall}>
+                <FormControlLabel value="daily" control={<Radio
+                    checked={selectedSchedule === "daily"}
+                    onChange={handleScheduleChange}
+                    value="daily"
+                />} label="täglich" />
             </div>
             <Divider />
-            <div className={classes.centerBox}>
-                <FormControl component="fieldset" disabled>
-                    <FormGroup>
-                        <FormControlLabel
-                            control={<Checkbox name="gilad" />}
-                            label="montags"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox name="jason" />}
-                            label="dienstags"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox name="antoine" />}
-                            label="mittwochs"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox name="gilad" />}
-                            label="donnerstags"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox name="jason" />}
-                            label="freitags"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox name="antoine" />}
-                            label="samstags"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox name="antoine" />}
-                            label="sonntags"
-                        />
-                    </FormGroup>
-                </FormControl>
+            <div className={classes.paddingSmall}>
+                <FormControlLabel value="weekly" control={<Radio
+                    checked={selectedSchedule === "weekly"}
+                    onChange={handleScheduleChange}
+                    value="weekly"
+                />} label="wöchentlich" />
+                {selectedSchedule === "weekly" &&
+                    <WeekdayCheckboxes />
+                }
             </div>
             <Divider />
             <div className={classes.paddingSmall} >
-                <MuiPickersUtilsProvider utils={DateFnsUtils} locale={de}>
-                    <KeyboardDatePicker
-                        inputVariant="outlined"
-                        disablePast
-                        disableToolbar
-                        variant="inline"
-                        format="dd/MM/yyyy"
-                        margin="normal"
-                        label="Datum festlegen"
-                        value={selectedDate}
-                        onChange={handleDateChange}
-                        KeyboardButtonProps={{
-                            'aria-label': 'change date',
-                        }}
-                    />
-                </MuiPickersUtilsProvider>
+                <FormControlLabel value="onDate" control={<Radio
+                    checked={selectedSchedule === "onDate"}
+                    onChange={handleScheduleChange}
+                    value="onDate"
+                />} label="Datum auswählen" />
+                {selectedSchedule === "onDate" &&
+                    <DateInputField date={selectedDate} handler={handleDateChange} />
+                }
             </div>
             <Divider />
             <div className={classes.paddingSmall} >
-                <MuiPickersUtilsProvider utils={DateFnsUtils} locale={de}>
-                    <KeyboardTimePicker
-                        inputVariant="outlined"
-                        ampm={false}
-                        margin="normal"
-                        label="Uhrzeit festlegen"
-                        value={selectedDate}
-                        onChange={handleDateChange}
-                        KeyboardButtonProps={{
-                            'aria-label': 'change time',
-                        }}
-                    />
-                </MuiPickersUtilsProvider>
+                <TimeInputField date={selectedDate} handler={handleDateChange} />
             </div>
-            <Divider />
+            <GreyDivider />
             <div className={classes.paddingSmall}>
                 <ContinueButton>
                     WEITER
