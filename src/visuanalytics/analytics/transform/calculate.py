766d60938631bc38a8cac0121be7063876d5fee3
Max Stephan <max.stephan@mni.thm.de>
2020-06-08 11:08:15 +0200

    calculate: hinzugefügt

---
+++ src/visuanalytics/analytics/transform/calculate.py
@@ -0,0 +1,12 @@
+def calculate_mean(values, data):
+    assert False, "Not implemented"
+
+
+def calculate_max(values, data):
+    assert False, "Not implemented"
+
+
+CALCULATE_ACTIONS = {
+    "mean": calculate_mean,
+    "max": calculate_max
+}
