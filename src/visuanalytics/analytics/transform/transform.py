29b9faff9162a7802e52a6275fc1bcdb666b6e0f
Max Stephan <max.stephan@mni.thm.de>
2020-06-08 09:34:40 +0200

    transform types: verschoben: gegenseitige imports lösen

--- src/visuanalytics/analytics/transform/transform.py
+++ src/visuanalytics/analytics/transform/transform.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from visuanalytics.analytics.control.procedures.step_data import StepData
-from visuanalytics.analytics.transform.transform_types import TRANSFORM_TYPES
 
 
 def transform(values: dict, data: StepData):
@@ -83,5 +82,19 @@ def transform_loop(values: dict, data: StepData):
 
 
 def transform_get_new_keys(values, idx, entry):
-    new_entry = values["new_keys"][idx] if values.get("new_keys") else entry
-    return new_entry
+    return values["new_keys"][idx] if values.get("new_keys") else entry
+
+
+TRANSFORM_TYPES = {
+    "transform_array": transform_array,
+    "select": transform_select,
+    "select_range": transform_select_range,
+    "append": transform_append,
+    "add_symbol": transform_add_symbol,
+    "replace": transform_replace,
+    "alias": transform_alias,
+    "date_format": transform_date_format,
+    "date_weekday": transform_date_weekday,
+    "date_now": transform_date_now,
+    "loop": transform_loop
+}
