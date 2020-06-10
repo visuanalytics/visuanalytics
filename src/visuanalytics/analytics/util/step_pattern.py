ef61f37a32dcfc911e057c91bb8691d0631c8ed2
Max Stephan <max.stephan@mni.thm.de>
2020-06-10 19:29:23 +0200

    step_pattern insert: wenn key nicht vorhanden erstellen

--- src/visuanalytics/analytics/util/step_pattern.py
+++ src/visuanalytics/analytics/util/step_pattern.py
@@ -16,6 +16,13 @@ def _to_int(x):
     return int(x) if x.isnumeric() else x
 
 
+def _get_or_create(d, k):
+    if not operator.contains(d, k):
+        operator.setitem(d, k, {})
+
+    return operator.getitem(d, k)
+
+
 def data_get_pattern(keys, data, split_key="|"):
     if isinstance(keys, str):
         keys = map(_to_int, keys.split(split_key))
@@ -30,7 +37,7 @@ def data_insert_pattern(keys, data, value, split_key="|"):
         keys = map(_to_int, key_array[:-1])
         last = _to_int(key_array[-1])
 
-        reduce(operator.getitem, keys, data)[last] = value
+        reduce(_get_or_create, keys, data)[last] = value
     else:
         data[keys] = value
 
