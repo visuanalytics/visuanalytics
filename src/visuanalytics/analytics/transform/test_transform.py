9c185299117c99e265e801296865a6496a8a5f9f
Max Stephan <max.stephan@mni.thm.de>
2020-06-12 11:54:05 +0200

    transform_test: laden der daten aus einer datei hinzugefügt

--- src/visuanalytics/analytics/transform/test_transform.py
+++ src/visuanalytics/analytics/transform/test_transform.py
@@ -1,5 +1,6 @@
 # TODO remove, File ist just for testting all transform funktions without use auf other funktions
 import json
+import time
 
 from visuanalytics.analytics.control.procedures.step_data import StepData
 from visuanalytics.analytics.transform.transform import transform
@@ -10,14 +11,19 @@ def test_transform(config_name, data: dict):
     with resources.open_resource(config_name) as fp:
         values = json.loads(fp.read())
 
-    step_data = StepData({})
+    step_data = StepData({}, "1")
     step_data.init_data(data)
 
     transform(values, step_data)
     print(f"Data after Transform: {step_data.data}")
 
 
+with resources.open_resource("exampledata/example_weather.json") as fp:
+    api_data = json.loads(fp.read())
+
 # Die daten die eigentlich aus der api kommen
-data = {"_req": [{"test": [2, 3.4, 8, 9]}, {"test": [2.4, 1.4, 8, 1]}]}
+t1 = time.time()
+data = {"_req": api_data}
 print(f"Data: {data}")
 test_transform("steps/example.json", data)
+print(time.time() - t1)
