a7eed386537efd2b03e42df7d5db268fa6998ef9
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-06-14 21:27:13 +0200

    JobItem hinzugefügt

--- src/frontend/src/JobList/index.tsx
+++ src/frontend/src/JobList/index.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
 import {Param} from "../util/param";
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import EditIcon from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
-import SettingsIcon from '@material-ui/icons/Settings';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import {useStyles} from "./style";
-import {ExpansionPanelSummary} from "./style";
-import {renderParamField} from "../util/renderParamFields";
-import {Button, Fade, Modal} from "@material-ui/core";
-import Backdrop from '@material-ui/core/Backdrop';
-import {ScheduleSelection} from "../JobCreate/ScheduleSelection";
-import {ContinueButton} from "../JobCreate/ContinueButton";
+import {JobItem} from "./JobItem";
 
 export interface Job {
     id: string;
@@ -30,14 +13,7 @@ export interface Job {
 
 export const JobList: React.FC = () => {
     // const jobInfo: Job[] = useFetch("/jobs");
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState<string | false>(false);
-    const [state, setState] = React.useState({
-        edit: true,
-        editIcon: 'block',
-        doneIcon: 'none'
-    });
-    const [open, setOpen] = React.useState(false);
+
     const jobInfo: Job[] = [
         {
             "id": "1",
@@ -69,127 +45,20 @@ export const JobList: React.FC = () => {
         }
     ]
 
-    const renderJobItem = (job: Job) => {
-        const paramInfo: Param[] = job.params;
 
-        const renderTextField = () => {
-            return (
-                <div>
-                    <div>
-                        <TextField
-                            className={classes.inputFields}
-                            id='standard-read-only-input'
-                            label="Thema"
-                            defaultValue={job.topic}
-                            InputProps={{
-                                readOnly: state.edit,
-                            }}
-                        />
-                    </div>
-                    <div>
-                        <Button className={classes.inputButton} onClick={handleOpen}>
-                            <TextField
-                                className={classes.inputFields}
-                                id='standard-read-only-input'
-                                label="Zeitplan"
-                                defaultValue={job.schedule}
-                                InputProps={{
-                                    readOnly: state.edit,
-                                }}
-                            />
-                        </Button>
-
-                    </div>
-                    <div>
-                        <TextField
-                            className={classes.inputFields}
-                            id='standard-read-only-input'
-                            label="nächstes Video"
-                            defaultValue={job.next}
-                            InputProps={{
-                                readOnly: true,
-                            }}
-                        />
-                    </div>
-                </div>
-            )
-        }
-        const handleOpen = () => {
-            setOpen(true);
-        };
-
-        const handleClose = () => {
-            setOpen(false);
-        };
-
-        const handleChange = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
-            setExpanded(isExpanded ? panel : false);
-        };
-        const handleEditClick = () => {
-            setState({edit: false, editIcon: 'none', doneIcon: 'block'});
-            setExpanded(job.id);
-        }
-        const handleDoneClick = () => {
-            setState({edit: true, editIcon: 'block', doneIcon: 'none'});
-        }
-        return (
-            <div className={classes.root}>
-                <ExpansionPanel expanded={expanded === job.id} onChange={handleChange(job.id)}>
-                    <ExpansionPanelSummary>
-                        {expanded ? <ExpandLess className={classes.expIcon}/> :
-                            <ExpandMore className={classes.expIcon}/>}
-                        <Typography className={classes.heading}>#{job.id} {job.name}</Typography>
-                        <IconButton className={classes.button} onClick={(event) => event.stopPropagation()}>
-                            <EditIcon style={{display: state.editIcon}} onClick={handleEditClick}/>
-                            <CheckCircleIcon style={{display: state.doneIcon}} onClick={handleDoneClick}/>
-                        </IconButton>
-                        <IconButton className={classes.button} onClick={(event) => event.stopPropagation()}>
-                            <SettingsIcon/>
-                        </IconButton>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <Grid item md={6}>
-                            {renderTextField()}
-                        </Grid>
-                        <Modal
-                            aria-labelledby="transition-modal-title"
-                            aria-describedby="transition-modal-description"
-                            className={classes.modal}
-                            open={open}
-                            onClose={handleClose}
-                            closeAfterTransition
-                            BackdropComponent={Backdrop}
-                            BackdropProps={{
-                                timeout: 500,
-                            }}
-                        >
-                            <Fade in={open}>
-                                <div className={classes.backdropContent}>
-                                    <ScheduleSelection/>
-                                    <ContinueButton>SPEICHERN</ContinueButton>
-                                </div>
-                            </Fade>
-                        </Modal>
-                        <Grid item md={6}>
-                            <div>
-                                {paramInfo.map(p =>
-                                    <div>
-                                        {renderParamField(p, classes, state.edit)}
-                                    </div>)
-                                }
-                            </div>
-                        </Grid>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-            </div>
-        );
-    }
 
     return (
         <>
             {jobInfo.map(j =>
                 <div>
-                    {renderJobItem(j)}
+                    <JobItem
+                        id={j.id}
+                        name={j.name}
+                        topic={j.topic}
+                        schedule={j.schedule}
+                        next={j.next}
+                        params={j.params}
+                    />
                 </div>)
             }
         </>
