b9fa0f558cb628d07eb45a4e3b8dba07a0114a19
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-06-14 21:26:17 +0200

    Schriftart aller Components geändert

--- src/frontend/src/App/index.tsx
+++ src/frontend/src/App/index.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { ComponentProvider } from "../ComponentProvider";
-import { Main } from "./Main";
-import { Header } from "../Header";
+import {ComponentProvider} from "../ComponentProvider";
+import {Main} from "./Main";
+import {Header} from "../Header";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: 'Dosis',
+    },
+});
 
 const App = () => {
-  return (
-    <div>
-      <ComponentProvider>
-        <Header />
-        <Main />
-      </ComponentProvider>
-    </div>
-  );
+    return (
+        <div>
+            <MuiThemeProvider theme={theme}>
+                <ComponentProvider>
+                    <Header/>
+                    <Main/>
+                </ComponentProvider>
+            </MuiThemeProvider>
+        </div>
+    );
 };
 
 export default App;
