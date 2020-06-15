bb639ffbcfdba17df8242f95d85a5e654971cfc9
David Martschenko <david.martschenko@web.de>
2020-06-15 12:44:46 +0200

    Bestätigung nach Job erstellen hinzugefügt

--- src/frontend/src/JobCreate/index.tsx
+++ src/frontend/src/JobCreate/index.tsx
@@ -30,6 +30,7 @@ export default function JobCreate() {
     // states for stepper logic
     const [activeStep, setActiveStep] = React.useState(0);
     const [selectComplete, setSelectComplete] = React.useState(false);
+    const [finished, setFinished] = React.useState(false);
 
     // states for topic selection logic
     const [selectedTopic, setSelectedTopic] = React.useState("");
@@ -47,6 +48,12 @@ export default function JobCreate() {
         date: new Date(),
         time: new Date()
     });
+
+    useEffect(() => {
+        if (activeStep === 3) {
+            setFinished(true);
+        }
+    }, [activeStep])
 
     // when selected topic changes, fetch new parameter list
     useEffect(() => {
@@ -193,7 +200,7 @@ export default function JobCreate() {
                     />
                 )
             default:
-                return 'Unknown step';
+                return "";
         }
     }
 
@@ -211,22 +218,31 @@ export default function JobCreate() {
                 })}
             </Stepper>
             <div className={classes.jobCreateBox}>
-                <div>
-                    <h3 className={classes.jobCreateHeader}>{descriptions[activeStep]}</h3>
-                </div>
-                <GreyDivider />
-                {getSelectPanel(activeStep)}
-                <GreyDivider />
-                <div className={classes.paddingSmall}>
-                    <span>
-                        <BackButton onClick={handleBack} style={{ marginRight: 20 }} disabled={activeStep <= 0}>
-                            {"Zurück"}
-                        </BackButton>
-                        <ContinueButton onClick={handleNext} style={{ marginLeft: 20 }} disabled={!selectComplete}>
-                            {activeStep < steps.length - 1 ? "WEITER" : "ERSTELLEN"}
-                        </ContinueButton>
-                    </span>
-                </div>
+                {!finished
+                    ?
+                    <div>
+                        <div>
+                            <h3 className={classes.jobCreateHeader}>{descriptions[activeStep]}</h3>
+                        </div>
+                        <GreyDivider />
+                        {getSelectPanel(activeStep)}
+                        <GreyDivider />
+                        <div className={classes.paddingSmall}>
+                            <span>
+                                <BackButton onClick={handleBack} style={{ marginRight: 20 }} disabled={activeStep <= 0}>
+                                    {"Zurück"}
+                                </BackButton>
+                                <ContinueButton onClick={handleNext} style={{ marginLeft: 20 }} disabled={!selectComplete}>
+                                    {activeStep < steps.length - 1 ? "WEITER" : "ERSTELLEN"}
+                                </ContinueButton>
+                            </span>
+                        </div>
+                    </div>
+                    :
+                    <div className={classes.paddingSmall}>
+                        Der Job wurde erstellt!
+                    </div>
+                }
             </div>
         </div>
     );
