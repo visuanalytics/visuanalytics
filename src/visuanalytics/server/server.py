47bd493b42ea4ce869607f22fdefddb9f74bf4dd
David Martschenko <david.martschenko@web.de>
2020-06-20 15:28:01 +0200

    Route-Methoden in eigenes Modul ausgelagert + Doc-Strings hinzugefügt

--- src/visuanalytics/server/server.py
+++ src/visuanalytics/server/server.py
@@ -36,50 +36,11 @@ def create_app():
     mimetypes.add_type("text/javascript", ".js")
 
     # Register the Blueprints
-    api.register_all(app)
+    app.register_blueprint(api.api)
 
     # Serve index.html
     @app.route("/", methods=["GET"])
     def index():
         return render_template("index.html")
-
-    # Serve topic list
-    @app.route("/topics", methods=["GET"])
-    def topics():
-        return "topic list"
-        # TODO retrieve actual topic list
-
-    # Serve parameter information for a given topic
-    # GET-Parameter: "topic"
-    @app.route("/params", methods=["GET"])
-    def params():
-        topic = request.args.get("topic")
-        return "params for topic: " + topic
-        # TODO: retrieve actual parameter list
-
-    # Serve job list
-    @app.route("/jobs", methods=["GET"])
-    def jobs():
-        return "jobs"
-        # TODO: retrieve actual job list
-
-    # Add job
-    @app.route("/add", methods=["POST"])
-    def add():
-        job = request.json
-        # TODO: add data base entry for the new job
-
-    # Edit job
-    @app.route("/edit", methods=["POST"])
-    def edit():
-        job = request.json
-        return "edit"
-        # TODO: update data base entry with the given job id
-
-    # Remove job
-    @app.route("/remove", methods=["POST"])
-    def remove():
-        job = request.json
-        # TODO: remove job from data base
 
     return app
