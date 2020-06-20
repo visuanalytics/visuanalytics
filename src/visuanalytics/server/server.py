1226a46d9776ab6bbf86133e5786cf6ca7d827f0
David Martschenko <david.martschenko@web.de>
2020-06-20 12:39:55 +0200

    End-Points definiert

--- src/visuanalytics/server/server.py
+++ src/visuanalytics/server/server.py
@@ -4,7 +4,7 @@ Enthält die Startkonfiguration für den Flask-Server.
 """
 import mimetypes
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from visuanalytics.server.api import api
 
@@ -39,8 +39,47 @@ def create_app():
     api.register_all(app)
 
     # Serve index.html
-    @app.route('/')
+    @app.route("/", methods=["GET"])
     def index():
         return render_template("index.html")
+
+    # Serve topic list
+    @app.route("/topics", methods=["GET"])
+    def topics():
+        return "topic list"
+        # TODO retrieve actual topic list
+
+    # Serve parameter information for a given topic
+    # GET-Parameter: "topic"
+    @app.route("/params", methods=["GET"])
+    def params():
+        topic = request.args.get("topic")
+        return "params for topic: " + topic
+        # TODO: retrieve actual parameter list
+
+    # Serve job list
+    @app.route("/jobs", methods=["GET"])
+    def jobs():
+        return "jobs"
+        # TODO: retrieve actual job list
+
+    # Add job
+    @app.route("/add", methods=["POST"])
+    def add():
+        job = request.json
+        # TODO: add data base entry for the new job
+
+    # Edit job
+    @app.route("/edit", methods=["POST"])
+    def edit():
+        job = request.json
+        return "edit"
+        # TODO: update data base entry with the given job id
+
+    # Remove job
+    @app.route("/remove", methods=["POST"])
+    def remove():
+        job = request.json
+        # TODO: remove job from data base
 
     return app
