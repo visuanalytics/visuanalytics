3e819bbbb714cfcd75fb4eec347ac550e3196435
Max Stephan <max.stephan@mni.thm.de>
2020-06-21 10:28:37 +0200

    step_data save_loop, save_loop_key: public -> private

--- src/visuanalytics/analytics/control/procedures/step_data.py
+++ src/visuanalytics/analytics/control/procedures/step_data.py
@@ -16,23 +16,23 @@ class StepData(object):
         return config_manager.get_private()["api_keys"][api_key_name]
 
     @staticmethod
-    def save_loop(idx, current, values: dict):
+    def __save_loop(idx, current, values: dict):
         values["_loop_states"] = {**values.get("_loop_states", {}), **{"_idx": idx, "_loop": current}}
         return idx, current
 
-    def save_loop_key(self, key, values: dict, ):
+    def __save_loop_key(self, key, values: dict, ):
         values["_loop_states"] = {**values.get("_loop_states", {}),
                                   **{"_key": self.get_data(key, values)}}
         return key
 
     def loop_array(self, loop_root: list, values: dict):
-        return map(lambda value: self.save_loop(value[0], value[1], values), enumerate(loop_root))
+        return map(lambda value: self.__save_loop(value[0], value[1], values), enumerate(loop_root))
 
     def loop_dict(self, loop_root: dict, values: dict):
-        return map(lambda value: self.save_loop(value[0], value[1], values), loop_root.items())
+        return map(lambda value: self.__save_loop(value[0], value[1], values), loop_root.items())
 
     def loop_key(self, keys: list, values: dict):
-        return map(lambda value: (value[0], self.save_loop_key(value[1], values)), enumerate(keys))
+        return map(lambda value: (value[0], self.__save_loop_key(value[1], values)), enumerate(keys))
 
     @property
     def data(self):
@@ -79,7 +79,7 @@ class StepData(object):
         # if value_string is int just return value
         if isinstance(value_string, numbers.Number):
             return value_string
-        
+
         if values is None:
             values = {}
 
