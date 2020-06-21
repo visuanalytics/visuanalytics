1cbcc2a2cb082ee73bb4b84f9b00f6bc855ddbd6
JannikLapp <64197016+JannikLapp@users.noreply.github.com>
2020-06-21 13:46:45 +0200

    if / else umgedreht

--- src/visuanalytics/analytics/processing/image/pillow/overlay.py
+++ src/visuanalytics/analytics/processing/image/pillow/overlay.py
@@ -52,13 +52,13 @@ def add_image(overlay: dict, source_img, draw, presets: dict, step_data: StepDat
         icon = icon.resize([step_data.format(overlay["size_x"]),
                             step_data.format(overlay["size_y"])], Image.LANCZOS)
     if overlay.get("transparency", False):
-        source_img.paste(icon, (step_data.format(overlay["pos_x"]),
-                                step_data.format(overlay["pos_y"])), icon)
-    else:
         source_img.alpha_composite(icon, (step_data.format(overlay["pos_x"]),
                                           step_data.format(overlay["pos_y"])))
-
-
+    else:
+        source_img.paste(icon, (step_data.format(overlay["pos_x"]),
+                                step_data.format(overlay["pos_y"])), icon)
+  
+        
 def add_image_array(overlay: dict, source_img, draw, presets: dict, step_data: StepData):
     for idx, i in enumerate(overlay["pos_x"]):
         if isinstance(overlay["colour"], list):
