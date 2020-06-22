914202323fd9440d3168353db2c58f0aa51ebd23
Max Stephan <max.stephan@mni.thm.de>
2020-06-22 09:15:15 +0200

    api request-multiple: Fehler behoben

--- src/visuanalytics/analytics/apis/api.py
+++ src/visuanalytics/analytics/apis/api.py
@@ -75,8 +75,8 @@ def request_multiple_custom(values: dict, data: StepData, name):
     if values.get("use_loop_as_key", False):
         data_dict = {}
 
-        for idx, value in enumerate(values["requests"]):
-            data_dict[value] = _api(values["requests"][idx], data, name)
+        for idx, key in enumerate(values["steps_value"]):
+            data_dict[key] = _api(values["requests"][idx], data, name)
         return data_dict
 
     data_array = []
