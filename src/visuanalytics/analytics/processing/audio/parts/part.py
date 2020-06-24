157dfb6a5f8c368a402588cb37cbc2c1b6c8fe7e
Max Stephan <max.stephan@mni.thm.de>
2020-06-24 11:26:59 +0200

    audio: type_utils verwenden

--- src/visuanalytics/analytics/processing/audio/parts/part.py
+++ src/visuanalytics/analytics/processing/audio/parts/part.py
@@ -1,4 +1,5 @@
 from visuanalytics.analytics.util.step_errors import AudioError, raise_step_error
+from visuanalytics.analytics.util.type_utils import get_type_func, register_type_func
 
 AUDIO_PARTS_TYPES = {}
 
@@ -6,14 +7,11 @@ AUDIO_PARTS_TYPES = {}
 @raise_step_error(AudioError)
 def audio_parts(values, data):
     # TODO raise TypeError
-    return "".join([f"{AUDIO_PARTS_TYPES[value['type']](value, data)} " for value in values])
+    return "".join([f"{get_type_func(value, AUDIO_PARTS_TYPES)(value, data)} " for value in values])
 
 
 def register_audio_parts(func):
-    func = raise_step_error(AudioError)(func)
-
-    AUDIO_PARTS_TYPES[func.__name__] = func
-    return func
+    return register_type_func(AUDIO_PARTS_TYPES, AudioError, func)
 
 
 @register_audio_parts
