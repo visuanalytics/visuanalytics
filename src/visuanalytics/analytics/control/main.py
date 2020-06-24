f3e712895151e36f194144c8943184074d337652
David Martschenko <david.martschenko@web.de>
2020-06-24 13:52:08 +0200

    Schreibfehler korrigiert

--- src/visuanalytics/analytics/control/main.py
+++ src/visuanalytics/analytics/control/main.py
@@ -3,7 +3,7 @@ import os
 
 from visuanalytics.analytics.control.scheduler.DbScheduler import DbScheduler
 from visuanalytics.analytics.control.scheduler.JsonScheduler import JsonScheduler
-from visuanalytics.analytics.util import resources, external_programms, config_manager
+from visuanalytics.analytics.util import resources, external_programs, config_manager
 from visuanalytics.server.db import db
 from visuanalytics.server import server
 
@@ -18,7 +18,7 @@ def main():
 
     app.run()
 
-    # If db is in use Start DbScheduler else run JsonScheduler
+    # if db is in use start DbScheduler else run JsonScheduler
     if config["db"]["use"]:
         DbScheduler(config["steps_base_config"]).start()
     else:
@@ -26,14 +26,14 @@ def main():
 
 
 def init(config: dict):
-    # Check if all external Programmes are installed
-    external_programms.all_installed(config.get("external_programms"))
+    # check if all external programs are installed
+    external_programs.all_installed(config.get("external_programs"))
 
     # initialize logging
     level = logging.INFO if config.get("testing", False) else logging.WARNING
     logging.basicConfig(format='%(module)s %(levelname)s: %(message)s', level=level)
 
-    # if db is in use Inizalisize Database
+    # if db is in use initialize database
     if config["db"]["use"]:
         # init db
         db.init_db()
@@ -41,7 +41,7 @@ def init(config: dict):
     # create temp dir
     os.makedirs(resources.get_resource_path("temp"), exist_ok=True)
 
-    # create out und instance dir
+    # create out and instance dir
     out_dir = config.get("steps_base_config", {}).get("output_path", "out")
     os.makedirs(resources.path_from_root(out_dir), exist_ok=True)
     os.makedirs(resources.path_from_root("instance"), exist_ok=True)
