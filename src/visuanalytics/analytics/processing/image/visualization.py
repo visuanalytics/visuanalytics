847029f45ea81bf3e141225d49575a6f6f5abd32
Max Stephan <max.stephan@mni.thm.de>
2020-06-24 11:26:48 +0200

    images: type_utils verwenden

--- src/visuanalytics/analytics/processing/image/visualization.py
+++ src/visuanalytics/analytics/processing/image/visualization.py
@@ -1,7 +1,8 @@
 from visuanalytics.analytics.control.procedures.step_data import StepData
 from visuanalytics.analytics.processing.image.pillow.pillow import generate_image_pillow
 from visuanalytics.analytics.processing.image.wordcloud.wordcloud import generate_image_wordcloud
-from visuanalytics.analytics.util.step_errors import raise_step_error
+from visuanalytics.analytics.util.step_errors import raise_step_error, ImageError
+from visuanalytics.analytics.util.type_utils import get_type_func
 
 IMAGE_TYPES = {
     "pillow": generate_image_pillow,
@@ -9,16 +10,15 @@ IMAGE_TYPES = {
 }
 
 
-# TODO (max) may change functions (in pillow, wordcloud and _generate_image)
-#  to can be used with raise_step_error (values, data)
-
-
-@raise_step_error(ImportError)
+@raise_step_error(ImageError)
 def generate_all_images(values: dict, step_data: StepData):
     for key, item in enumerate(values["images"]):
         path = _generate_image(values["images"][item], values["images"], values["presets"], step_data)
         values["images"][item] = path
 
 
+@raise_step_error(ImageError)
 def _generate_image(image: dict, prev_paths, presets: dict, step_data: StepData):
-    return IMAGE_TYPES[image["type"]](image, prev_paths, presets, step_data)
+    image_func = get_type_func(image, IMAGE_TYPES)
+
+    return image_func(image, prev_paths, presets, step_data)
