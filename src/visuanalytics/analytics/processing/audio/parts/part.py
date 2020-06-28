50a7433b94178a42f189243310db21e416a1b40d
Tanja265 <tanja.gutsche@mni.thm.de>
2020-06-28 10:52:46 +0200

    audio_parts hinzugefügt

--- src/visuanalytics/analytics/processing/audio/parts/part.py
+++ src/visuanalytics/analytics/processing/audio/parts/part.py
@@ -1,3 +1,5 @@
+from numpy import random
+
 from visuanalytics.analytics.util.step_errors import AudioError, raise_step_error
 from visuanalytics.analytics.util.type_utils import get_type_func, register_type_func
 
@@ -17,3 +19,39 @@ def register_audio_parts(func):
 @register_audio_parts
 def text(values, data):
     return data.format(values["pattern"], values)
+
+
+@register_audio_parts
+def option_for(values, data):
+    """Führt Funktionen aus.
+
+    :param values: Werte aus der JSON-Datei
+    :param data: Daten aus der API
+    """
+
+    value = data.get_data(values["check"], values)
+    condition = data.get_data(values["condition"], values)
+
+    if condition == value:
+        values["transform"] = values.get("on_equal", [])
+    elif condition > value:
+        values["transform"] = values.get("on_higher", [])
+    elif condition < value:
+        values["transform"] = values.get("on_lower", [])
+
+    return random_text(values, data)
+
+
+@register_audio_parts
+def random_text(values, data):
+    """Sucht aus mehreren Texten einen aus.
+
+    :param values: Werte aus der JSON-Datei
+    :param data: Daten aus der API
+    """
+    len_pattern = len(values["pattern"])
+    choice = []
+    for i in range(len_pattern):
+        choice.append(i)
+    rand = random.choice(choice)
+    return data.format(values["pattern"][rand], values)
