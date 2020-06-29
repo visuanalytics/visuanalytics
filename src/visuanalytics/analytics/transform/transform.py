edfcade4697e9e04e08281abbca17d53cb59e7f6
jannik-pc-linux <jannik.lapp@mni.thm.de>
2020-06-29 10:05:22 +0200

    small fix for entry rank is missing

--- src/visuanalytics/analytics/transform/transform.py
+++ src/visuanalytics/analytics/transform/transform.py
@@ -188,10 +188,9 @@ def get_equivalent_key(values: dict, data: StepData):
         new_key = get_new_keys(values, idx)
         index = data.format("{_idx}", values)
         name = data.data["_req"]["Tabelle"][int(index)]["TeamInfoId"]
-        rank = data.data["_req"]["Tabelle"][int(index)]["Rank"]
         for item in data.data["_req"]["Vorherige-Tabelle"]:
             if item["TeamInfoId"] == name:
-                new_values = int(data.format("{_idx}", values)) - int(rank)
+                new_values = int(data.format("{_idx}", values)) - int(index)
                 break
         data.insert_data(new_key, int(new_values), values)
 
