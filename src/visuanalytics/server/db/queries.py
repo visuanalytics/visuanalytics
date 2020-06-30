7ca61d43e31e16316294fa01d65ceda9bc0ce026
David Martschenko <david.martschenko@web.de>
2020-06-30 17:12:23 +0200

    DB-Abfrage für Job-Liste hinzugefügt

--- src/visuanalytics/server/db/queries.py
+++ src/visuanalytics/server/db/queries.py
@@ -6,12 +6,9 @@ STEPS_LOCATION = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../
 
 
 def get_topic_names():
-    topic_names = []
     con = db.open_con()
-    res = con.execute("SELECT NAME FROM steps")
-    for row in res:
-        topic_names.append(row["name"])
-    return topic_names
+    res = con.execute("SELECT steps_name FROM steps")
+    return [row["steps_name"] for row in res]
 
 
 def get_params(topic_id):
@@ -26,11 +23,40 @@ def get_params(topic_id):
 
 
 def get_job_list():
-    job_list = []
     con = db.open_con()
-    res = con.execute("SELECT * FROM jobs JOIN job_schedule JOIN schedule")
-    for row in res:
-        job = {}
-        schedule = {}
-        schedule[""]
-        job["job_id"] = row["id"]
+    res = con.execute("""
+    SELECT DISTINCT 
+    job_id, job_name, daily, weekly, on_date, date, time, steps_name,
+    group_concat(weekday) AS weekdays,
+    group_concat(DISTINCT key || ":"  || value) AS params
+    FROM job 
+    INNER JOIN steps USING (steps_id)
+    LEFT JOIN job_config USING (job_id)
+    INNER JOIN schedule USING (schedule_id) 
+    LEFT JOIN schedule_weekday USING (schedule_id) 
+    GROUP BY (job_id);
+    """)
+
+    return [row_to_job(row) for row in res]
+
+
+def row_to_job(row):
+    params_string = str(row["params"])
+    key_values = [kv.split(":") for kv in params_string.split(",")] if params_string != "None" else []
+    params = [{"name": kv[0], "selected": kv[1], "possibleValues": []} for kv in
+              key_values]  # TODO (David): possibleValues
+    weekdays = str(row["weekdays"]).split(",")
+    return {
+        "jobId": row["Job_id"],
+        "jobName": row["job_name"],
+        "topicName": row["steps_name"],
+        "params": params,
+        "schedule": {
+            "daily": row["daily"],
+            "weekly": row["weekly"],
+            "onDate": row["on_date"],
+            "date": row["date"],
+            "time": row["time"],
+            "weekdays": weekdays
+        }
+    }
