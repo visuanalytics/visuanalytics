0490b7abfd0470c9b160f4ed103207205ac6c175
Max Stephan <max.stephan@mni.thm.de>
2020-07-03 19:27:28 +0200

    audio: audio Type custom hinzugefügt

--- src/visuanalytics/analytics/processing/audio/audio.py
+++ src/visuanalytics/analytics/processing/audio/audio.py
@@ -1,10 +1,16 @@
+import base64
+import mimetypes
+
 import gtts.tokenizer.symbols
 from gtts import gTTS
 
+from visuanalytics.analytics.apis.api import api_request
 from visuanalytics.analytics.control.procedures.step_data import StepData
 from visuanalytics.analytics.processing.audio.parts import part
 from visuanalytics.analytics.util import resources
-from visuanalytics.analytics.util.step_errors import raise_step_error, AudioError
+from visuanalytics.analytics.util.config_manager import get_config
+from visuanalytics.analytics.util.step_errors import raise_step_error, AudioError, InvalidContentTypeError
+from visuanalytics.analytics.util.step_pattern import data_get_pattern
 from visuanalytics.analytics.util.type_utils import get_type_func, register_type_func
 
 GENERATE_AUDIO_TYPES = {}
@@ -25,19 +31,10 @@ def get_audio_config(values: dict, data: StepData):
 
 @raise_step_error(AudioError)
 def generate_audios(values: dict, data: StepData):
-    audios = values["audio"]["audios"]
-    config = values["audio"]["config"]
-    config["type"] = "default"  # TODO (max) defualt or get custom config
+    config: dict = get_audio_config(values, data)
 
-    for key in audios:
-        audios[key] = generate_audio(audios[key], data, config)
-
-
-@raise_step_error(AudioError)
-def generate_audio(values: dict, data: StepData, config: dict):
-    values["text"] = part.audio_parts(values["parts"], data)
-
-    return get_type_func(config, GENERATE_AUDIO_TYPES)(values, data, config)
+    audio_func = get_type_func(config, GENERATE_AUDIO_TYPES)
+    audio_func(values, data, config)
 
 
 def register_generate_audio(func):
@@ -46,21 +43,75 @@ def register_generate_audio(func):
 
 @register_generate_audio
 def default(values: dict, data: StepData, config: dict):
-    sub_pairs = config.get("sub_pairs", None)
+    audios = values["audio"]["audios"]
 
-    if sub_pairs:
-        for key in sub_pairs:
-            value = data.get_data(key, values)
-            gtts.tokenizer.symbols.SUB_PAIRS.append((key, value))
+    for key in audios:
+        text = part.audio_parts(audios[key]["parts"], data)
 
-    tts = gTTS(values["text"], lang=config["lang"])
+        sub_pairs = data.format_json(config.get("sub_pairs", None), None, values)
 
-    file_path = resources.new_temp_resource_path(data.data["_pipe_id"], config["format"])
-    tts.save(file_path)
+        if sub_pairs:
+            for key in sub_pairs:
+                value = data.get_data(key, values)
+                gtts.tokenizer.symbols.SUB_PAIRS.append((key, value))
 
-    return file_path
+        tts = gTTS(text, lang=data.format(config["lang"]))
+
+        file_path = resources.new_temp_resource_path(data.data["_pipe_id"], data.format(config["format"]))
+        tts.save(file_path)
+
+        audios[key] = file_path
 
 
 @register_generate_audio
 def custom(values: dict, data: StepData, config: dict):
-    assert False, "Not implemented"
+    _prepare_custom(config.get("prepare", None), data, config)
+
+    audios = values["audio"]["audios"]
+
+    for key in audios:
+        text = part.audio_parts(audios[key]["parts"], data)
+
+        data.data["_audio"]["text"] = text
+        # TODO set Testing to false
+        generate = config["generate"]
+        generate["include_headers"] = True
+        response = api_request(generate, data, "audio")
+
+        audios[key] = _save_audio(response, data, config)
+
+
+def _prepare_custom(values: dict, data: StepData, config: dict):
+    data.data["_audio"] = {}
+
+    if values is not None:
+        # TODO(Max) Solve better
+        data.data["_audio"]["pre"] = api_request(values, data, "audio")
+
+
+def _save_audio(response, data: StepData, config: dict):
+    content_type = response["headers"]["content-type"]
+    post_generate = config.get("post_generate", {})
+    audio = response["content"]
+
+    # If Content Type is json Try to decode json String with base64
+    if content_type.startswith("application/json"):
+        # Get Audio String
+        audio = data_get_pattern(data.format(post_generate["audio_key"]), audio)
+        # decode Audio Key with base64
+        audio = base64.b64decode(audio)
+        audio_path = resources.new_temp_resource_path(data.data["_pipe_id"],
+                                                      data.format(post_generate["file_extension"]))
+    else:
+        # Check if Content type is an audio Type
+        if not content_type.startswith("audio"):
+            raise InvalidContentTypeError(None, content_type, "'audio/*'")
+
+        # Get File Extention from Mime Type:
+        extension = mimetypes.guess_all_extensions(content_type)
+        audio_path = resources.new_temp_resource_path(data.data["_pipe_id"], extension[0].replace(".", ""))
+
+    with open(audio_path, "wb") as fp:
+        fp.write(audio)
+
+    return audio_path
