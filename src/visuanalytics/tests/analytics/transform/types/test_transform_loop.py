82f4f7b18b8fa5c3d49d57c35d1d46d6d963929b
Tanja265 <tanja.gutsche@mni.thm.de>
2020-07-06 10:09:05 +0200

    unfertig test_transform_loop.py

--- src/visuanalytics/tests/analytics/transform/types/test_transform_loop.py
+++ src/visuanalytics/tests/analytics/transform/types/test_transform_loop.py
@@ -4,33 +4,75 @@ from visuanalytics.tests.analytics.transform.transform_test_helper import prepar
 
 
 class TestTransformLoop(unittest.TestCase):
+    # TODO
     def setUp(self):
-        self.data = {
-            "test": [
-                "hallo",
-                "test",
-                "hallo2"
-            ]
-        }
+        self.data = [
+            {"hallo": 1},
+            {"hallo": 4},
+            {"hallo": 5},
+        ]
 
-    def test_loop_through(self):
+    def test_looped_all(self):
         values = [
             {
-                "type": "loop",
-                "array_key": "_req|test",
-                "loop": ["hallo", "test"]
+                "type": "transform_array",
+                "array_key": "_req|0",
+                "transform": [
+                    {
+                        "type": "loop",
+                        "range_start": 0,
+                        "range_stop": 2,
+                        "transform": [
+                            {
+                                "type": "add_symbol",
+                                "keys": ["_loop|0"],
+                                "pattern": "test"
+                            }
+                        ]
+                    }
+                ]
             }
         ]
 
         expected_data = {
-            "_req": {
-                "test": [
-                    "hallo",
-                    "test",
-                    "hallo2"
-                ]
-            }
+            "_req": [
+                {"hallo": "test"},
+                {"hallo": "test"},
+                {"hallo": "test"},
+            ]
         }
 
         exp, out = prepare_test(values, self.data, expected_data)
-        self.assertDictEqual(exp, out, "Loop through Failed")
+        self.assertDictEqual(exp, out, "loop_all Failed")
+
+    def test_loop_idx(self):
+        values = [
+            {
+                "type": "transform_array",
+                "array_key": "_req",
+                "transform": [
+                    {
+                        "type": "loop",
+                        "values": "{_idx}",
+                        "transform": [
+                            {
+                                "type": "add_symbol",
+                                "keys": ["_loop|0"],
+                                "pattern": "{_key}"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+
+        expected_data = {
+            "_req": [
+                {"hallo": 0},
+                {"hallo": 1},
+                {"hallo": 2},
+            ]
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "loop_idx Failed")
