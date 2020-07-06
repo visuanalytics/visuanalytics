eaa3f86a979073bc894df5e02992ad2d336ebded
David Martschenko <david.martschenko@web.de>
2020-07-06 12:31:47 +0200

    Neue DB-Query-Methoden für DB-Scheduler hinzugefügt

--- src/visuanalytics/server/db/job.py
+++ src/visuanalytics/server/db/job.py
@@ -122,3 +122,40 @@ def get_steps(job_id: int):
     with db.open_con() as con:
         return con.execute("select name from job as j, steps as s where j.id = ?and j.steps == s.id",
                            [job_id]).fetchone()
+
+
+def get_job_schedules():
+    """ Gibt alle angelegten jobs mitsamt ihren Zeitplänen zurück.
+
+    """
+    with db.open_con() as con:
+        res = con.execute("""
+        SELECT DISTINCT job_id, daily, weekly, on_date, date, time, group_concat(DISTINCT weekday) AS weekdays
+        FROM job 
+        INNER JOIN schedule USING(schedule_id) 
+        LEFT JOIN schedule_weekday USING(schedule_id)
+        GROUP BY(job_id)
+        """).fetchall()
+        return res
+
+
+def get_job_run_info(job_id):
+    """Gibt den Namen eines Jobs, dessen Parameter sowie den Namen der zugehörigen steps-Json-Datei zurück.
+
+    :param job_id: id des Jobs
+    """
+    print("DB query")
+    with db.open_con() as con:
+        res = con.execute("""
+        SELECT job_name, json_file_name, key, value
+        FROM job 
+        INNER JOIN steps USING(steps_id) 
+        LEFT JOIN job_config USING(job_id) 
+        WHERE job_id=?
+        """, [job_id]).fetchall()
+        job_name = res[0]["job_name"]
+        steps_name = res[0]["json_file_name"]
+        print(res)
+        config = {row["key"]: row["value"] for row in res}
+        print(job_name, steps_name, config)
+        return job_name, steps_name, config
