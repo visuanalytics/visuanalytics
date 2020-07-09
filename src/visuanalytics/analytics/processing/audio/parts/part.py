2c61919d83f0a0e771883cc85c57c8fa1bc432b4
Max Stephan <max.stephan@mni.thm.de>
2020-07-09 19:03:37 +0200

    audio part: Fehler in compare und option behoben

--- src/visuanalytics/analytics/processing/audio/parts/part.py
+++ src/visuanalytics/analytics/processing/audio/parts/part.py
@@ -9,7 +9,6 @@ AUDIO_PARTS_TYPES = {}
 
 @raise_step_error(AudioError)
 def audio_parts(values, data):
-    # TODO raise TypeError
     return "".join([f"{get_type_func(value, AUDIO_PARTS_TYPES)(value, data)} " for value in values])
 
 
@@ -31,39 +30,32 @@ def text(values, data):
 
 @register_audio_parts
 def compare(values, data):
-    """Vergleicht zwei Werte miteinander und führt je nachdem ob =, < oder > random_text mit angegebenen Werten durch.
+    """Vergleicht zwei Werte miteinander und führt je nachdem ob =, < oder > die "audio_parts"-Funktionen aus.
 
-    Vergleicht zwei Werte miteinander und führt random_text mit den jeweils unter on_equal, on_higher oder on_lower
-    angegebenen Werten durch.
-
-    Wenn condition-Value gleich check-Value, führe random_text für die Werte unter on_equal durch.
-    Wenn condition-Value größer check-Value, führe random_text für die Werte unter on_higher durch.
-    Wenn condition-Value kleiner check-Value, führe random_text für die Werte unter on_lower durch.
+    Wenn condition-Value gleich check-Value, führe"audio_parts"-Funktionen aus on_equal durch.
+    Wenn condition-Value größer check-Value, führe "audio_parts"-Funktionen aus on_higher durch.
+    Wenn condition-Value kleiner check-Value, führe "audio_parts"-Funktionen aus on_lower durch.
 
     :param values: Werte aus der JSON-Datei
     :param data: Daten aus der API
     """
 
-    values["pattern"] = values["transform"] = execute_type_compare(values, data)
-
-    random_text(values, data)
+    return audio_parts(execute_type_compare(values, data), data)
 
 
 @register_audio_parts
 def option(values, data):
-    """Führt die `"random_text"` aus, je nachdem ob ein bestimmter Wert `"true"` oder `"false"` ist.
+    """Führt die aufgeführten `"audio_parts"`-Funktionen aus, je nachdem ob ein bestimmter Wert `"true"` oder `"false"` ist.
 
-    Wenn der Wert, der in `"check"` steht `"true"` ist, wird `"random_text"` ausgeführt mit den pattern,
-    die unter `"on_true"` stehen.
-    Wenn der Wert, der in `"check"` steht `"false"` ist, wird `"random_text"` ausgeführt mit den pattern,
-    die unter `"on_false"` stehen.
+     Wenn der Wert, der in `"check"` steht `"true"` ist, werden die `"audio_parts"`-Funktionen ausgeführt,
+     die unter `"on_true"` stehen.
+     Wenn der Wert, der in `"check"` steht `"false"` ist, werden die `"audio_parts"`-Funktionen ausgeführt,
+     die unter `"on_false"` stehen.
 
     :param values: Werte aus der JSON-Datei
     :param data: Daten aus der API
     """
-    values["pattern"] = execute_type_option(values, data)
-
-    random_text(values, data)
+    return audio_parts(execute_type_option(values, data), data)
 
 
 @register_audio_parts
