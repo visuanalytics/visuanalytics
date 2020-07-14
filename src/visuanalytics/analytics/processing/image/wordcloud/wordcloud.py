1c68f6716aebe2852e2b3ccb0cc8e9fd075c5952
Max Stephan <max.stephan@mni.thm.de>
2020-07-14 13:31:00 +0200

    wordcloud: einige Verbesserungen

--- src/visuanalytics/analytics/processing/image/wordcloud/wordcloud.py
+++ src/visuanalytics/analytics/processing/image/wordcloud/wordcloud.py
@@ -8,6 +8,29 @@ from wordcloud import WordCloud, STOPWORDS
 from visuanalytics.analytics.control.procedures.step_data import StepData
 from visuanalytics.analytics.util import resources
 
+WORDCLOUD_DEFAULT_PARAMETER = {
+    "background_color": "white",
+    "width": 400,
+    "height": 200,
+    "collocations": True,
+    "max_font_size": None,
+    "max_words": 200,
+    "contour_width": 0,
+    "contour_color": "white",
+    "font_path": None,
+    "prefer_horizontal": 0.90,
+    "scale": 1,
+    "min_font_size": 4,
+    "font_step": 1,
+    "mode": "RGB",
+    "relative_scaling": 0.5,
+    "color_func": None,
+    "regexp": None,
+    "colormap": "viridis",
+    "normalize_plurals": True,
+    "stopwords": None
+}
+
 
 def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
     """
@@ -26,66 +49,43 @@ def wordcloud(image: dict, prev_paths, presets: dict, step_data: StepData):
     :return: Den Pfad zum erstellten Bild
     :rtype: str
     """
-
-    WORDCLOUD_DEFAULT_PARAMETER = {
-        "background_color": "white",
-        "width": 400,
-        "height": 200,
-        "collocations": True,
-        "max_font_size": None,
-        "max_words": 200,
-        "contour_width": 0,
-        "contour_color": "white",
-        "font_path": None,
-        "prefer_horizontal": 0.90,
-        "scale": 1,
-        "min_font_size": 4,
-        "font_step": 1,
-        "mode": "RGB",
-        "relative_scaling": 0.5,
-        "color_func": None,
-        "regexp": None,
-        "colormap": "viridis",
-        "normalize_plurals": True,
-        "stopwords": None
-    }
     wordcloud_parameter = WORDCLOUD_DEFAULT_PARAMETER
+    parameter = image.get("parameter", {})
 
-    for each in WORDCLOUD_DEFAULT_PARAMETER:
-        if each in image["parameter"]:
-            wordcloud_parameter[each] = step_data.format(image["parameter"][each])
+    for each in wordcloud_parameter:
+        if each in parameter:
+            wordcloud_parameter[each] = step_data.format(parameter[each])
 
-    if step_data.get_data(image["parameter"]["color_func"], image) == True:
+    if step_data.get_data_bool(parameter.get("color_func", False), {}):
         wordcloud_parameter["color_func"] = color_func
 
-    if image["parameter"]["mask"] is not None and image["parameter"]["mask"]["figure"] is not None:
-        x0 = step_data.format(image["parameter"]["mask"]["x"])
-        y0 = step_data.format(image["parameter"]["mask"]["y"])
+    if parameter["mask"] is not None and parameter["mask"]["figure"] is not None:
+        x0 = step_data.format(parameter["mask"]["x"])
+        y0 = step_data.format(parameter["mask"]["y"])
         x, y = np.ogrid[:x0, :y0]
 
-        figure = step_data.get_data(image["parameter"]["mask"]["figure"], image)
+        figure = step_data.get_data(parameter["mask"]["figure"], {})
         if figure == "circle":
             mask = (x - (x0 / 2)) ** 2 + (y - (y0 / 2)) ** 2 > 400 ** 2
         elif figure == "square":
             mask = x * y
-        elif figure == "own_mask":
+        else:
             # TODO testen
-            path = resources.get_image_path(image["parameter"]["mask"]["path"])
+            path = resources.get_image_path(parameter["mask"]["path"])
             mask = np.array(Image.open(path))
 
         wordcloud_parameter["mask"] = 255 * mask.astype(int)
     else:
-        mask = None
-        wordcloud_parameter["mask"] = mask
+        wordcloud_parameter["mask"] = None
 
     stopwords = set(STOPWORDS)
 
-    dont_use = step_data.get_data(image["stopwords"], image)
+    dont_use = step_data.get_data(image["stopwords"], {})
     stopwords.add(dont_use)
     list_dont_use = dont_use.split()
     STOPWORDS.update(list_dont_use)
 
-    wordcloud_image = WordCloud(**wordcloud_parameter).generate(step_data.get_data(image["text"], image))
+    wordcloud_image = WordCloud(**wordcloud_parameter).generate(step_data.get_data(image["text"], {}))
 
     plt.axis("off")
     image = wordcloud_image.to_image()
