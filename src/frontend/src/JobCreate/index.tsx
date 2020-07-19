429414d98bad8fda4cbdf54697b105c9614b107e
David Martschenko <david.martschenko@web.de>
2020-07-19 22:45:52 +0200

    Import entfernt

--- src/frontend/src/JobCreate/index.tsx
+++ src/frontend/src/JobCreate/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
