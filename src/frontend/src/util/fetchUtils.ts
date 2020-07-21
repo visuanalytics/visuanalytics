7ed7e6d33b11686d4955a00e44c83f16e38e9624
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-07-21 17:35:42 +0200

    images hinzugefügt

--- src/frontend/src/util/fetchUtils.ts
+++ src/frontend/src/util/fetchUtils.ts
@@ -10,5 +10,5 @@ export function getUrl(url: string) {
   if (process.env.REACT_APP_VA_SERVER_URL)
     return process.env.REACT_APP_VA_SERVER_URL + "/visuanalytics" + url;
 
-  return url;
+  return "/visuanalytics" + url;
 }
