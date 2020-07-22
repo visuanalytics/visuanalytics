b3c641a0071c916289014c2c9f6cbde681e6ba76
jannik-pc-linux <jannik.lapp@mni.thm.de>
2020-07-22 11:41:33 +0200

    fixed saving bug

--- src/visuanalytics/analytics/storing/storing.py
+++ src/visuanalytics/analytics/storing/storing.py
@@ -3,6 +3,7 @@ Modul schreibt Dicts in JSON Files
 """
 
 import json
+import copy
 
 from visuanalytics.analytics.control.procedures.step_data import StepData
 from visuanalytics.analytics.util.step_errors import raise_step_error, StoringError
@@ -26,14 +27,26 @@ def storing(values: dict, data: StepData):
                     with resources.open_specific_memory_resource(data.get_data("_conf|job_name", values),
                                                                  value["name"]) as fp:
                         old_data = json.loads(fp.read())
-                    if old_data == data.get_data(value["key"], values):
+                    new_data = data.get_data(value["key"], values)
+                    new_data = _remove_keys(value, new_data)
+                    if old_data == new_data:
                         continue
                 except (FileNotFoundError, IndexError):
                     pass
             with open(resources.new_memory_resource_path(data.get_data("_conf|job_name", values), value["name"]),
                       'w') as fp:
                 export_data = data.get_data(value["key"], data.data)
-                for key in value.get("except", None):
-                    del export_data[key]
-                print(export_data)
+                export_data = _remove_keys(value, export_data)
                 json.dump(export_data, fp)
+
+
+def _remove_keys(value, export_data):
+    if value.get("except", None) is not None:
+        export_data = copy.deepcopy(export_data)
+        for key in value["except"]:
+            for idx, row in enumerate(export_data):
+                try:
+                    del export_data[idx][key]
+                except KeyError:
+                    pass
+    return export_data
