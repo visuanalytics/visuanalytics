fc463b827da3590854095b438dedb9a939899d31
Jannik-PC <jannik.lapp@mni.thm.de>
2020-07-23 10:18:55 +0200

    fix for pull request #557

--- src/visuanalytics/analytics/storing/storing.py
+++ src/visuanalytics/analytics/storing/storing.py
@@ -22,26 +22,25 @@ def storing(values: dict, data: StepData):
     """
     if values.get("storing", None):
         for value in values["storing"]:
+            new_data = _remove_keys(value, data.get_data(value["key"], values))
             if value.get("safe_only_on_change", True):
                 try:
                     with resources.open_specific_memory_resource(data.get_data("_conf|job_name", values),
                                                                  value["name"]) as fp:
                         old_data = json.loads(fp.read())
-                    new_data = _remove_keys(value, data.get_data(value["key"], values))
                     if old_data == new_data:
                         continue
                 except (FileNotFoundError, IndexError):
                     pass
             with open(resources.new_memory_resource_path(data.get_data("_conf|job_name", values), value["name"]),
                       'w') as fp:
-                export_data = _remove_keys(value, data.get_data(value["key"], data.data))
-                json.dump(export_data, fp)
+                json.dump(new_data, fp)
 
 
 def _remove_keys(value, export_data):
-    if value.get("except", None) is not None:
+    if value.get("exclude", None) is not None:
         export_data = copy.deepcopy(export_data)
-        for key in value["except"]:
+        for key in value["exclude"]:
             for idx, row in enumerate(export_data):
                 try:
                     del export_data[idx][key]
