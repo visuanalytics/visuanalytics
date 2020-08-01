4634f6f74bdf7b28f6ef2c91922109e493539fc6
Max Stephan <max.stephan@mni.thm.de>
2020-08-01 09:43:34 +0200

    dict_utils: todo hinzugefügt

--- src/visuanalytics/util/dict_utils.py
+++ src/visuanalytics/util/dict_utils.py
@@ -1,4 +1,5 @@
 def merge_dict(dict1, dict2):
+    # TODO (max) may merge sub lists
     for k in set(dict1).union(set(dict2)):
         if k in dict2:
             if isinstance(dict1.get(k, None), dict) and isinstance(dict2[k], dict):
