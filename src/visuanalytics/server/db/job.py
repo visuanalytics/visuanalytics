40b05a5e50fafbbbd5cf578bee5d16e7d8ebccf0
Max Stephan <max.stephan@mni.thm.de>
2020-08-05 22:43:56 +0200

    db job: Funktionen zum hinzufügen/bearbeiten der logs hinzugefügt

--- src/visuanalytics/server/db/job.py
+++ src/visuanalytics/server/db/job.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from visuanalytics.server.db import db
 
 
@@ -36,3 +38,24 @@ def get_job_run_info(job_id):
         config = {row["key"]: row["value"] for row in res}
 
         return job_name, steps_name, config
+
+
+def insert_log(job_id: int, state: int, start_date: datetime):
+    with db.open_con() as con:
+        con.execute("INSERT INTO job_logs(job_id, state, start_date) values (?, ?, ?)", [job_id, state, start_date])
+        id = con.execute("SELECT last_insert_rowid() as id").fetchone()
+        con.commit()
+
+        return id["id"]
+
+
+def update_log_error(id: int, state: int, error_msg: str):
+    with db.open_con() as con:
+        con.execute("UPDATE job_logs SET state = (?), error_msg = ? where job_logs_id = (?)", [state, error_msg, id])
+        con.commit()
+
+
+def update_log_finish(id: int, state: int, duration: int):
+    with db.open_con() as con:
+        con.execute("UPDATE job_logs SET state = ?, duration = ?  where job_logs_id = ?", [state, duration, id])
+        con.commit()
