bd6ebf074a05f48dd2e919606089f8d99dd34ce6
Jannik-PC <jannik.lapp@mni.thm.de>
2020-08-07 10:28:16 +0200

    delte memory files count hinzugefügt

--- src/visuanalytics/analytics/storing/storing.py
+++ src/visuanalytics/analytics/storing/storing.py
@@ -7,6 +7,7 @@ import copy
 
 from visuanalytics.analytics.control.procedures.step_data import StepData
 from visuanalytics.analytics.util.step_errors import raise_step_error, StoringError
+from visuanalytics.analytics.util.video_delete import delete_memory_files
 from visuanalytics.util import resources
 
 
@@ -35,6 +36,8 @@ def storing(values: dict, data: StepData):
             with open(resources.new_memory_resource_path(data.get_data("_conf|job_name", values), value["name"]),
                       'w') as fp:
                 json.dump(new_data, fp)
+            delete_memory_files(data.get_data("_conf|job_name", values),
+                                value["name"], value.get("count", 10))
 
 
 def _remove_keys(value, export_data):
