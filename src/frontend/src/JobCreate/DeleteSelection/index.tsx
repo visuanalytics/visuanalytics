3a748f2374f089dd1fba5727098bfb8f506c1662
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-08-08 15:59:26 +0200

    DeleteSelection an deleteScheduler angepasst

--- src/frontend/src/JobCreate/DeleteSelection/index.tsx
+++ src/frontend/src/JobCreate/DeleteSelection/index.tsx
@@ -1,60 +1,86 @@
 import React from "react";
 import {useStyles} from "../style";
 import {Collapse, Divider, Fade, FormControlLabel, Radio, TextField} from "@material-ui/core";
-import {Schedule} from "../../util/schedule";
+import {DayHour, DeleteSchedule} from "../../util/deleteSchedule";
+import {DayHourInputField} from "../../util/DayHourInput";
 
 interface DeleteSelectionProps {
-    schedule: Schedule,
-    selectScheduleHandler: (schedule: Schedule) => void;
+    deleteSchedule: DeleteSchedule,
+    selectDeleteScheduleHandler: (deleteSchedule: DeleteSchedule) => void;
 }
 
-export const DeleteSelection: React.FC<DeleteSelectionProps> = ({ schedule, selectScheduleHandler }) => {
+export const DeleteSelection: React.FC<DeleteSelectionProps> = ({ deleteSchedule, selectDeleteScheduleHandler }) => {
     const classes = useStyles();
 
-    const handleSelectNoDelete = () => {
-        selectScheduleHandler({ type: "noDelete", time: schedule.time})
+    const handleSelectNoDeletion = () => {
+        selectDeleteScheduleHandler({type: "noDeletion"})
     }
 
-    const handleSelectOnTimeDelete = () => {
-        selectScheduleHandler({ type: "onTime", removalTime: schedule.time, time: schedule.time})
+    const handleSelectOnDayHour = () => {
+        selectDeleteScheduleHandler({type: "onDayHour", removalTime:  {days: 1, hours: 1}})
     }
 
-    const handleSelectOldOnNew = () => {
-        selectScheduleHandler({ type: "oldOnNew", time: schedule.time})
+    const handleSelectKeepCount = () => {
+        selectDeleteScheduleHandler({type: "keepCount", keepCount: 1})
+    }
+
+    const handleSelectFixNames = () => {
+        selectDeleteScheduleHandler({type: "fixNames", count: 1})
+    }
+
+    const handleAddRemovalTime = (removalTime: DayHour) => {
+        if (deleteSchedule.type === "onDayHour") {
+            selectDeleteScheduleHandler({...deleteSchedule, removalTime: removalTime})
+        }
+    }
+
+    const handleAddKeepCount = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (deleteSchedule.type === "keepCount") {
+            selectDeleteScheduleHandler({...deleteSchedule, keepCount: Number(event.target.value)})
+        }
+    }
+
+    const handleAddFixNames = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (deleteSchedule.type === "fixNames") {
+            selectDeleteScheduleHandler({...deleteSchedule, count: Number(event.target.value)})
+        }
     }
 
     return (
         <Fade in={true}>
             <div>
-                <div className={classes.paddingSmall}>
+                <div className={classes.MPaddingTB}>
                     <div className={classes.centerDiv}>
-                        <FormControlLabel value="nodelete" control={<Radio
-                            checked={schedule.type === "noDelete"}
-                            onChange={handleSelectNoDelete}
-                            value="nodelete"
+                        <FormControlLabel value="noDeletion" control={<Radio
+                            checked={deleteSchedule.type === "noDeletion"}
+                            onChange={handleSelectNoDeletion}
+                            value="noDeletion"
                         />} label="nicht löschen" />
                     </div>
                 </div>
                 <Divider />
-                <div className={classes.paddingSmall}>
+                <div className={classes.MPaddingTB}>
                     <div className={classes.centerDiv}>
-                        <FormControlLabel value="deleteOld" control={<Radio
-                            checked={schedule.type === "oldOnNew"}
-                            onChange={handleSelectOldOnNew}
-                            value="deleteOld"
-                        />} label="Video bei Neugenerierung löschen" />
+                        <FormControlLabel value="onDayHour" control={<Radio
+                            checked={deleteSchedule.type === "onDayHour"}
+                            onChange={handleSelectOnDayHour}
+                            value="onDayHour"
+                        />} label="Video nach bestimmter Zeit löschen" />
                     </div>
+                    <Collapse in={deleteSchedule.type === "onDayHour"}>
+                        <DayHourInputField handler={handleAddRemovalTime} />
+                    </Collapse>
                 </div>
                 <Divider />
-                <div className={classes.paddingSmall} >
+                <div className={classes.MPaddingTB} >
                     <div className={classes.centerDiv}>
-                        <FormControlLabel value="ontime" control={<Radio
-                            checked={schedule.type === "onTime"}
-                            onChange={handleSelectOnTimeDelete}
-                            value="ontime"
-                        />} label="nach Tagen" />
+                        <FormControlLabel value="keepCount" control={<Radio
+                            checked={deleteSchedule.type === "keepCount"}
+                            onChange={handleSelectKeepCount}
+                            value="keepCount"
+                        />} label="nach bestimmter Anzahl von Videos löschen" />
                     </div>
-                    <Collapse in={schedule.type === "onTime"}>
+                    <Collapse in={deleteSchedule.type === "keepCount"}>
                         <TextField
                             label="Tage"
                             type="number"
@@ -62,6 +88,28 @@ export const DeleteSelection: React.FC<DeleteSelectionProps> = ({ schedule, sele
                                 shrink: true,
                             }}
                             variant="outlined"
+                            onChange={handleAddKeepCount}
+                        />
+                    </Collapse>
+                </div>
+                <Divider />
+                <div className={classes.MPaddingTB} >
+                    <div className={classes.centerDiv}>
+                        <FormControlLabel value="fixNames" control={<Radio
+                            checked={deleteSchedule.type === "fixNames"}
+                            onChange={handleSelectFixNames}
+                            value="fixNames"
+                        />} label="bestimmte Anzahl an Videos mit fixem Namen erstellen" />
+                    </div>
+                    <Collapse in={deleteSchedule.type === "fixNames"}>
+                        <TextField
+                            label="Tage"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                            onChange={handleAddFixNames}
                         />
                     </Collapse>
                 </div>
