4bec6a3589cba9caa2319f282d72821237425ddc
Tanja265 <tanja.gutsche@mni.thm.de>
2020-08-09 13:41:06 +0200

    Tests für to_dict, sort und length

---
+++ src/visuanalytics/tests/analytics/transform/types/test_transform_sort.py
@@ -0,0 +1,101 @@
+import unittest
+
+from visuanalytics.tests.analytics.transform.transform_test_helper import prepare_test
+
+
+class TestTransformSort(unittest.TestCase):
+    def setUp(self):
+        self.data = {
+            "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom", "Z"],
+            "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0]
+        }
+
+    def test_sort_a2z(self):
+        values = [
+            {
+                "type": "sort",
+                "keys": ["_req|test"],
+                "new_keys": ["_req|test1"]
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
+                         "Z"],
+                "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0],
+                "test1": ["Argentina", "Canada", "Cyprus", "Germany", "Norway", "Schweden", "United Kingdom", "USA",
+                          "Z"]
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "sort a2z failed")
+
+    def test_sort_z2a(self):
+        values = [
+            {
+                "type": "sort",
+                "keys": ["_req|test"],
+                "new_keys": ["_req|test1"],
+                "reverse": True
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
+                         "Z"],
+                "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0],
+                "test1": ["Z", "USA", "United Kingdom", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
+                          "Argentina"]
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "sort z2a failed")
+
+    def test_sort_ascending(self):
+        values = [
+            {
+                "type": "sort",
+                "keys": ["_req|numbers"],
+                "new_keys": ["_req|test1"]
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
+                         "Z"],
+                "numbers": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                "test1": ["Z", "USA", "United Kingdom", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
+                          "Argentina"]
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "sort ascending failed")
+
+    def test_sort_descending(self):
+        values = [
+            {
+                "type": "sort",
+                "keys": ["_req|numbers"],
+                "new_keys": ["_req|test1"],
+                "reverse": True
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
+                         "Z"],
+                "numbers": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+                "test1": ["Z", "USA", "United Kingdom", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
+                          "Argentina"]
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "sort descending failed")
---
+++ src/visuanalytics/tests/analytics/transform/types/test_transform_to_dict.py
@@ -0,0 +1,86 @@
+import unittest
+
+from visuanalytics.tests.analytics.transform.transform_test_helper import prepare_test
+
+
+class TestTransformToDict(unittest.TestCase):
+    def setUp(self):
+        self.data = {
+            "list": ["Canada", 5, "Argentina", 3, "Cyprus", 2, "Schweden", 1, "Norway", 4, "USA", 5, "Germany", 7,
+                     "United Kingdom", 8, "Z", 10],
+            "string": "Canada 5 Argentina 3 Cyprus 2 Schweden 1 Norway 4 USA 5 Germany 7 United Kingdom 8 Z 10",
+            "tuple": [("Canada", 5), ("Argentina", 3), ("Cyprus", 2), ("Schweden", 1), ("Norway", 4), ("USA", 5),
+                      ("Germany", 7), ("United Kingdom", 8), ("Z", 10)]
+        }
+
+    def test_to_dict_list(self):
+        values = [
+            {
+                "type": "to_dict",
+                "keys": ["_req|list"],
+                "new_keys": ["_req|dict_list"]
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "list": ["Canada", 5, "Argentina", 3, "Cyprus", 2, "Schweden", 1, "Norway", 4, "USA", 5, "Germany", 7,
+                         "United Kingdom", 8, "Z", 10],
+                "string": "Canada 5 Argentina 3 Cyprus 2 Schweden 1 Norway 4 USA 5 Germany 7 United Kingdom 8 Z 10",
+                "tuple": [("Canada", 5), ("Argentina", 3), ("Cyprus", 2), ("Schweden", 1), ("Norway", 4), ("USA", 5),
+                          ("Germany", 7), ("United Kingdom", 8), ("Z", 10)],
+                "dict_list": {"Canada": 5, "Argentina": 3, "Cyprus": 2, "Schweden": 1, "Norway": 4,
+                              "USA": 5, "Germany": 7, "United Kingdom": 8, "Z": 10}
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "to_dict list failed")
+
+    def test_to_dict_string(self):
+        values = [
+            {
+                "type": "to_dict",
+                "keys": ["_req|string"],
+                "new_keys": ["_req|dict_string"]
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "list": ["Canada", 5, "Argentina", 3, "Cyprus", 2, "Schweden", 1, "Norway", 4, "USA", 5, "Germany", 7,
+                         "United Kingdom", 8, "Z", 10],
+                "string": "Canada 5 Argentina 3 Cyprus 2 Schweden 1 Norway 4 USA 5 Germany 7 United Kingdom 8 Z 10",
+                "tuple": [("Canada", 5), ("Argentina", 3), ("Cyprus", 2), ("Schweden", 1), ("Norway", 4), ("USA", 5),
+                          ("Germany", 7), ("United Kingdom", 8), ("Z", 10)],
+                "dict_string": {"Canada": 5, "Argentina": 3, "Cyprus": 2, "Schweden": 1, "Norway": 4,
+                                "USA": 5, "Germany": 7, "United Kingdom": 8, "Z": 10}
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "to_dict string failed")
+
+    def test_to_dict_tuple(self):
+        values = [
+            {
+                "type": "to_dict",
+                "keys": ["_req|tuple"],
+                "new_keys": ["_req|dict_tuple"]
+            }
+        ]
+
+        expected_data = {
+            "_req": {
+                "list": ["Canada", 5, "Argentina", 3, "Cyprus", 2, "Schweden", 1, "Norway", 4, "USA", 5, "Germany", 7,
+                         "United Kingdom", 8, "Z", 10],
+                "string": "Canada 5 Argentina 3 Cyprus 2 Schweden 1 Norway 4 USA 5 Germany 7 United Kingdom 8 Z 10",
+                "tuple": [("Canada", 5), ("Argentina", 3), ("Cyprus", 2), ("Schweden", 1), ("Norway", 4), ("USA", 5),
+                          ("Germany", 7), ("United Kingdom", 8), ("Z", 10)],
+                "dict_tuple": {"Canada": 5, "Argentina": 3, "Cyprus": 2, "Schweden": 1, "Norway": 4,
+                               "USA": 5, "Germany": 7, "United Kingdom": 8, "Z": 10}
+            }
+        }
+
+        exp, out = prepare_test(values, self.data, expected_data)
+        self.assertDictEqual(exp, out, "to_dict tuple failed")
