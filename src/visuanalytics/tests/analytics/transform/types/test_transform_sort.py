59f5caca44df954fe414b0083554a12532023aa3
Tanja265 <tanja.gutsche@mni.thm.de>
2020-08-09 14:25:58 +0200

    Tests fertig für sort

--- src/visuanalytics/tests/analytics/transform/types/test_transform_sort.py
+++ src/visuanalytics/tests/analytics/transform/types/test_transform_sort.py
@@ -24,7 +24,7 @@ class TestTransformSort(unittest.TestCase):
                 "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
                          "Z"],
                 "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0],
-                "test1": ["Argentina", "Canada", "Cyprus", "Germany", "Norway", "Schweden", "United Kingdom", "USA",
+                "test1": ["Argentina", "Canada", "Cyprus", "Germany", "Norway", "Schweden", "USA", "United Kingdom",
                           "Z"]
             }
         }
@@ -39,6 +39,7 @@ class TestTransformSort(unittest.TestCase):
                 "keys": ["_req|test"],
                 "new_keys": ["_req|test1"],
                 "reverse": True
+
             }
         ]
 
@@ -47,7 +48,7 @@ class TestTransformSort(unittest.TestCase):
                 "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
                          "Z"],
                 "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0],
-                "test1": ["Z", "USA", "United Kingdom", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
+                "test1": ["Z", "United Kingdom", "USA", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
                           "Argentina"]
             }
         }
@@ -61,6 +62,7 @@ class TestTransformSort(unittest.TestCase):
                 "type": "sort",
                 "keys": ["_req|numbers"],
                 "new_keys": ["_req|test1"]
+
             }
         ]
 
@@ -68,9 +70,8 @@ class TestTransformSort(unittest.TestCase):
             "_req": {
                 "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
                          "Z"],
-                "numbers": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                "test1": ["Z", "USA", "United Kingdom", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
-                          "Argentina"]
+                "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0],
+                "test1": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             }
         }
 
@@ -91,9 +92,8 @@ class TestTransformSort(unittest.TestCase):
             "_req": {
                 "test": ["Canada", "Argentina", "Cyprus", "Schweden", "Norway", "USA", "Germany", "United Kingdom",
                          "Z"],
-                "numbers": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-                "test1": ["Z", "USA", "United Kingdom", "Schweden", "Norway", "Germany", "Cyprus", "Canada",
-                          "Argentina"]
+                "numbers": [10, 4, 2, 6, 7, 3, 1, 5, 9, 8, 0],
+                "test1": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
             }
         }
 
