b83a2bdccc1ec35bfe1808c010f9cf01d5b89993
Max Stephan <max.stephan@mni.thm.de>
2020-08-12 00:57:16 +0200

    Docs Technische_dokumentation: modul namen entfernt

--- Docs/conf.py
+++ Docs/conf.py
@@ -46,6 +46,8 @@ language = 'de'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**tests**']
 
+add_module_names = False
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
