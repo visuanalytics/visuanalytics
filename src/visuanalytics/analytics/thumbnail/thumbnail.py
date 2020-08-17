787a8f8801ea029430d2f30a389caa69c3e3dfa1
Tanja265 <tanja.gutsche@mni.thm.de>
2020-08-17 18:52:16 +0200

    docstrings und Kommentare sequence.py und thumbnail.py

--- src/visuanalytics/analytics/thumbnail/thumbnail.py
+++ src/visuanalytics/analytics/thumbnail/thumbnail.py
@@ -11,15 +11,15 @@ from visuanalytics.analytics.util.type_utils import register_type_func, get_type
 from visuanalytics.util import resources
 
 THUMBNAIL_TYPES = {}
-"""Ein Dictionary bestehende aus allen Thumbnail Typ Methoden."""
+"""Ein Dictionary bestehend aus allen Thumbnail-Typ-Methoden."""
 
 
 def register_thumbnail(func):
     """
     Fügt eine Typ-Funktion dem Dictionary THUMBNAIL_TYPES hinzu.
 
-    :param func: Eine Funktion
-    :return: Die übergebene Funktion
+    :param func: eine Funktion
+    :return: die übergebene Funktion
     """
     return register_type_func(THUMBNAIL_TYPES, ThumbnailError, func)
 
@@ -45,6 +45,12 @@ def thumbnail(values: dict, step_data: StepData):
 
 @register_thumbnail
 def new(values: dict, step_data: StepData):
+    """Erstellt ein neues Bild, welches als Thumbnail für das zu erstellende Video verwendet wird.
+
+    :param values: Werte aus der JSON-Datei
+    :param data: Daten aus der API
+    :return:
+    """
     image_func = get_type_func(values["thumbnail"]["image"], IMAGE_TYPES)
     src_file = image_func(values["thumbnail"]["image"], step_data, values["images"])
     _copy_and_rename(src_file, values, step_data)
@@ -52,6 +58,12 @@ def new(values: dict, step_data: StepData):
 
 @register_thumbnail
 def created(values: dict, step_data: StepData):
+    """Verwendet ein bereits erstelltes Bild als Thumbnail für das zu erstellende Video.
+
+    :param values: Werte aus der JSON-Datei
+    :param data: Daten aus der API
+    :return:
+    """
     src_file = values["images"][values["thumbnail"]["name"]]
     _copy_and_rename(src_file, values, step_data)
 
