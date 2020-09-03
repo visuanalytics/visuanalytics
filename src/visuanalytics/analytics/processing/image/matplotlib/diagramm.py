a0255db36e4f1e03eee80c836ee5bf8962e1f082
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-09-03 13:06:12 +0200

    Diagramm erweiterung

--- src/visuanalytics/analytics/processing/image/matplotlib/diagramm.py
+++ src/visuanalytics/analytics/processing/image/matplotlib/diagramm.py
@@ -1,16 +1,22 @@
 import matplotlib.pyplot as plt
 from visuanalytics.util import resources
+from datetime import datetime, timedelta
 
+def diagramm(len: int):
+    now = datetime.now()
 
+    cases = [300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 310]
+    days = []
 
-def diagramm():
-    cases = [300, 302, 302, 310, 313, 320, 321, 330, 300, 302, 302, 310, 313, 320]
-    days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    for x in range(len):
+        day = now - timedelta(days=x)
+        days.insert(0, day.strftime('%d. %b'))
 
     plt.rcParams.update({'font.size': 22})
-    fig = plt.figure(figsize=[10, 5])
+    fig = plt.figure(figsize=[10, 6])
     ax = fig.add_subplot(111)
     for axis in  ['top','bottom','left','right']:
         ax.spines[axis].set_linewidth(2)
     plt.bar(days, cases, color =(0.02, 0.36, 0.49, 1))
+    plt.xticks(rotation=45)
     plt.savefig(resources.new_temp_resource_path('test', "png"), transparent=True)
