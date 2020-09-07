7863b00883150cb1113134d3296c7cf66bd08316
Jannik-PC <jannik.lapp@mni.thm.de>
2020-09-07 10:36:55 +0200

    edited configs for diagramm

--- src/visuanalytics/analytics/processing/image/matplotlib/diagram.py
+++ src/visuanalytics/analytics/processing/image/matplotlib/diagram.py
@@ -9,19 +9,26 @@ def generate_diagram(values: dict, step_data: StepData, prev_paths):
     data = step_data.format(values["data"])
     data = data[1:len(data) - 1].split(",")
     data = list(map(float, data))
-    now = datetime.now()
     days = []
-
-    for x in range(len(data)):
-        day = now - timedelta(days=x)
-        days.insert(0, day.strftime('%d.%m'))
-    plt.rcParams.update({'font.size': 18})
-    fig = plt.figure(figsize=[9, 4.5])
+    if step_data.format(values.get("label_use_date", False)) is True:
+        now = datetime.now()
+        for x in range(len(data)):
+            day = now - timedelta(days=x)
+            days.insert(0, day.strftime('%d.%m'))
+    else:
+        days = values["label"]
+    plt.rcParams.update({'font.size': step_data.format(values.get("label_size", 18))})
+    fig = plt.figure(
+        figsize=[step_data.format(values.get("plot_size_x", 20)),
+                 step_data.format(values.get("plot_size_y", 10))])
     ax = fig.add_subplot(111)
     for axis in ['top', 'bottom', 'left', 'right']:
-        ax.spines[axis].set_linewidth(2)
-    plt.bar(days, data, color=(0.02, 0.36, 0.49, 1))
-    plt.xticks(rotation=45)
+        ax.spines[axis].set_linewidth(step_data.format(values.get("axis_depth", 1)))
+    plt.bar(days, data, color=(step_data.format(values["label_colour"].get("r", 0)),
+                               step_data.format(values["label_colour"].get("g", 0)),
+                               step_data.format(values["label_colour"].get("b", 0)),
+                               step_data.format(values["label_colour"].get("t", 1))))
+    plt.xticks(rotation=step_data.format(values.get("label_rotation", 0)))
     plt.tight_layout()
     file = resources.new_temp_resource_path(step_data.data["_pipe_id"], "png")
     plt.savefig(file, transparent=True)
