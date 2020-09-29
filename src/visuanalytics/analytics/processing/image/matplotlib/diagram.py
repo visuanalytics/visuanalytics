7b671dcdfc4771b3fec1ed15f4a1f95c8ce11c48
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-09-29 12:39:16 +0200

    Verbesserungen umgesetzt

--- src/visuanalytics/analytics/processing/image/matplotlib/diagram.py
+++ src/visuanalytics/analytics/processing/image/matplotlib/diagram.py
@@ -11,28 +11,53 @@ def generate_diagram(values: dict, step_data: StepData, prev_paths):
     data = data[1:len(data) - 1].split(", ")
     data = list(map(float, data))
     days = []
-    if step_data.format(values.get("label_use_date", False)) is True:
+    labels = None
+    if values.get("label_data", None) is not None:
+        labels = step_data.format(values["label_data"])
+        labels = labels[1:len(labels) - 1].split(", ")
+        labels = list(map(str, labels))
+    if step_data.format(values.get("label_use_date", False)):
         now = datetime.now()
         for hop_value in range(len(data)):
             day = now - timedelta(days=hop_value)
             days.insert(0, day.strftime('%d.%m'))
     else:
         days = step_data.format(values["label"])
+        days = days.replace("'", "")
         days = days[1:len(days) - 1].split(", ")
         days = list(map(str, days))
+        if values.get("label_append", None) is not None:
+            replace_cities = values["label_append"]
+            for idx, city in enumerate(days):
+                for replace_city in replace_cities:
+                    if city == replace_city:
+                        label_append_value = str(values["label_append_value"])
+                        label_append_value = label_append_value.replace("_idx", str(idx))
+                        days[idx] = city + " " + step_data.format(label_append_value)
     plt.rcParams.update({'font.size': step_data.format(values.get("label_size", 18))})
     fig = plt.figure(
         figsize=[step_data.format(values.get("plot_size_x", 20)),
                  step_data.format(values.get("plot_size_y", 10))])
-
     ax = fig.add_subplot(111)
+
+    if step_data.format(values.get("sorted", False)):
+        if labels is None:
+            days, data = zip(*sorted(zip(days, data)))
+        else:
+            days, data, labels = zip(*sorted(zip(days, data, labels)))
 
     for axis in ['top', 'bottom', 'left', 'right']:
         ax.spines[axis].set_linewidth(step_data.format(values.get("axis_depth", 1)))
 
+    if values.get("grid_axis", None) is not None:
+        ax.grid(axis=step_data.format(values["grid_axis"]), color=step_data.format(values.get("grid_color", "grey")))
     if step_data.format(values["plot_type"]) == "bar_chart":
         ax.set_yticks(np.arange(len(days)))
         ax.set_yticklabels(days)
+        if values.get("marked_label", None) is not None:
+            for l in ax.get_yticklabels():
+                if l.get_text() == step_data.format(values["marked_label"]):
+                    l.set_fontweight(550)
         ax.invert_yaxis()  # labels von oben nach unten
 
         bar_list = plt.barh(np.arange(len(days)), data,
@@ -50,7 +75,7 @@ def generate_diagram(values: dict, step_data: StepData, prev_paths):
     else:
         raise
 
-    if step_data.format(values.get("use_special_x_labels", False)) is True:
+    if step_data.format(values.get("use_special_x_labels", False)):
         current_value = step_data.format(values.get("x_label_start", 0))
         x_label_list = [current_value]
         max_value = max(data)
@@ -60,7 +85,7 @@ def generate_diagram(values: dict, step_data: StepData, prev_paths):
         while current_value < max_value:
             current_value = current_value + hop_value
             x_label_list.append(current_value)
-        if step_data.format(values.get("x_label_one_value_more", False)) is True:
+        if step_data.format(values.get("x_label_one_value_more", False)):
             x_label_list.append(current_value + hop_value)
         ax.set_xticks(x_label_list)
 
@@ -78,18 +103,20 @@ def generate_diagram(values: dict, step_data: StepData, prev_paths):
 
     if values.get("label_data", None) is not None:
         rects = ax.patches
-        labels = step_data.format(values["label_data"])
-        labels = labels[1:len(labels) - 1].split(", ")
-        labels = list(map(str, labels))
-        for rect, label in zip(rects, labels):
-            if step_data.format(values.get("use+-_label", False)) is True:
+        for rect, label, data, days in zip(rects, labels, data, days):
+            if step_data.format(values.get("use+-_label", False)):
                 if label[0] != '-' and float(label) != 0.0:
                     label = "+" + label
                 if float(label) == 0.0:
-                    label = "0"
-            ax.text(rect.get_width() + 0.4, (rect.get_y() + rect.get_height() / 2) + 0.2, label,
-                    fontsize=step_data.format(values["label_fontsize"]))
-
+                    label = "\u00B10"
+            label = str(data) + " / " + label
+            if values.get("marked_label", None) is not None:
+                if days == step_data.format(values["marked_label"]):
+                    ax.text(rect.get_width() + 0.4, (rect.get_y() + rect.get_height() / 2) + 0.2, label,
+                            fontsize=step_data.format(values["label_fontsize"]), fontweight=550)
+                else:
+                    ax.text(rect.get_width() + 0.4, (rect.get_y() + rect.get_height() / 2) + 0.2, label,
+                            fontsize=step_data.format(values["label_fontsize"]))
     file = resources.new_temp_resource_path(step_data.data["_pipe_id"], "png")
     plt.savefig(file, transparent=True)
     return file
