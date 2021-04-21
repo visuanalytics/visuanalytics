3943020bf4019310367da838486f7ca5f88fdd92
Janek Berg <janek.berg@mni.thm.de>
2021-04-21 13:35:22 +0200

    added file upload, basic filetype checking and backend-communication for import to TypeSelection component

--- src/frontend/src/CreateInfoProvider/typeSelection/index.tsx
+++ src/frontend/src/CreateInfoProvider/typeSelection/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 /*import {
     List,
     ListItem,
@@ -36,53 +36,106 @@ export const TypeSelection: React.FC<TypeSelectionProps>  = (props) => {
     const [newSource, setNewSource] = React.useState(false);
     //duplicate is necessary to disable both options at the beginning
     const [importSource, setImportSource] = React.useState(false);
+    //contains the uploaded file if the user uses the import function
+    const [file, setFile] = React.useState<File | null>(null);
+    //boolean flag to identify if the user has uploaded a file
+    const [fileSelected, setFileSelected] = React.useState(false);
+    //ref to read out the filename for basic type checking
+    const fileUploader = React.useRef<HTMLInputElement>(null)
+    /**
+     * Handler method for a successful import request to the backend.
+     * Gets the imported data and passes it to the parent.
+     */
+    const handleSuccess = (jsonData: any) => {
+        //TODO: set parent source data to received object
+    }
 
+    /**
+     * Handler for errors happening when requesting the backend.
+     * Will display an error message and not proceed.
+     */
+    const handleError = (err: Error) => {
+        //TODO: add error handling for communication failure
+    }
+
+    //create the method to be called for sending import file to the backend
+    const sendTestData = useCallFetch("/importSource", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json\n"
+            },
+            body: file
+        }, handleSuccess, handleError
+    );
+
+
+    /**
+     * Event handle method for file selection by the user.
+     * @param event The onChange-event for a new selected file
+     * Stores the selected file in state and sets the corresponding fileSelected-flag to true.
+     * Also checks if the filepath given by the browser has a .json-Extension as first type check.
+     */
+    const handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
+        if(event.target.files==null) {
+            //TODO: error handling on null file
+        } else {
+            setFile(event.target.files[0]);
+            setFileSelected(true);
+            if(fileUploader.current !== null) {
+                let splittedString = fileUploader.current.value.split(".")
+                if (splittedString[splittedString.length-1] !== "json") {
+                    //TODO: throw error since non-json file was selected
+                }
+            }
+        }
+    }
 
 
     /**
      * Handler method for the "proceed" button. Passes the users choice to parent component and then proceeds.
      */
     const handleProceed = () => {
-        //TODO: pass data to parent
+        //TODO: signal the choice to the parent
+        if(importSource) {
+            //TODO: File exchange with backend, send results to parent
+        }
         props.continueHandler();
     }
 
 
-    //TODO: find solution for a file upload button, possibly use external component since material-ui doesnt offer one
+    //TODO: find a prettier solution for a file upload button, possibly use external component since material-ui doesnt offer one
     //const components = React.useContext(ComponentContext);
     return (
         <div>
-            <FormControlLabel
-                control={
-                    <Checkbox checked={newSource} onChange={(e) => {if(newSource) setNewSource(false);
-                    else {setNewSource(true); setImportSource(false)}}}/>
-                }
-                label="Eine neue Datenquelle erstellen"
-            />
-            <br/>
-            <div>
+            <form>
                 <FormControlLabel
                     control={
-                        <Checkbox checked={importSource} onChange={(e) => {if(importSource) setImportSource(false);
-                            else {setImportSource(true); setNewSource(false)}}}/>
+                        <Checkbox checked={newSource} onChange={(e) => {if(newSource) setNewSource(false);
+                        else {setNewSource(true); setImportSource(false)}}}/>
                     }
-                    label="Eine bestehende Datenquelle importieren"
+                    label="Eine neue Datenquelle erstellen"
                 />
+                <br/>
                 <div>
-                    <input
-                        disabled={!importSource}
-                        type="file"
-                        accept=".json"
+                    <FormControlLabel
+                        control={
+                            <Checkbox checked={importSource} onChange={(e) => {if(importSource) setImportSource(false);
+                            else {setImportSource(true); setNewSource(false)}}}/>
+                        }
+                        label="Eine bestehende Datenquelle importieren"
                     />
+                    <div>
+                        <input ref={fileUploader} disabled={!importSource} type="file" accept=".json" onChange={handleFileSelect}/>
+                    </div>
                 </div>
-            </div>
-            <br/>
-            <Button variant="contained" size="large" onClick={props.backHandler}>
-                abbrechen
-            </Button>
-            <Button disabled={!(newSource||(importSource/*&&fileUploaded*/))} variant="contained" size="large" onClick={handleProceed}>
-                weiter
-            </Button>
+                <br/>
+                <Button variant="contained" size="large" onClick={props.backHandler}>
+                    abbrechen
+                </Button>
+                <Button disabled={!(newSource||(importSource&&fileSelected))} variant="contained" size="large" onClick={handleProceed}>
+                    weiter
+                </Button>
+            </form>
         </div>
     )
 };
