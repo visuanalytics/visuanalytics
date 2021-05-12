f553968f11e47b46873711f43968123a1f0fd788
Sören <LupusCanes1@gmail.com>
2021-05-12 14:10:47 +0200

    Changed modules to now also start historisation using infoproviders

--- src/visuanalytics/analytics/control/scheduler/DbScheduler.py
+++ src/visuanalytics/analytics/control/scheduler/DbScheduler.py
@@ -16,17 +16,22 @@ logger = logging.getLogger(__name__)
 
 
 class DbScheduler(Scheduler):
-    def __run_jobs(self, job_id):
-        job_name, json_file_name, config = job.get_job_run_info(str(job_id))
-        logger.info(f"Job {job_id}: '{job_name}' started")
-        self._start_job(job_id, job_name, json_file_name, config, True)
+    def __run_jobs(self, job_id, is_job):
+        if is_job:
+            job_name, json_file_name, config = job.get_job_run_info(str(job_id))
+            logger.info(f"Job {job_id}: '{job_name}' started")
+            self._start_job(job_id, job_name, json_file_name, config, True)
+        else:
+            infoprovider_name = job.get_infoprovider_run_info(str(job_id))
+            logger.info(f"Infoprovider {job_id}: '{infoprovider_name}' started")
+            self._start_infoprovider(job_id, infoprovider_name, True)
 
     @ignore_errors
-    def __check(self, row, now):
+    def __check(self, row, now, is_job):
         # check if type is "interval" and job has to be run
         if row["s_type"] == "interval":
-            if self._check_interval(now, get_interval(row), row["job_id"], True):
-                self.__run_jobs(row["job_id"])
+            if self._check_interval(now, get_interval(row), row["job_id" if is_job else "infoprovider_id"], True):
+                self.__run_jobs(row["job_id" if is_job else "infoprovider_id"], is_job)
             return
 
         # check if time is current Time
@@ -36,17 +41,17 @@ class DbScheduler(Scheduler):
         # check if date is today
         if row["s_type"] == "on_date" and row["date"] == now.date():
             # TODO Delete date schedule after run
-            self.__run_jobs(row["job_id"])
+            self.__run_jobs(row["job_id" if is_job else "infoprovider_id"], is_job)
             return
 
         # if type is "weekly" and check if weekday is same as today
         if row["s_type"] == "weekly" and now.weekday() in map(int, row["weekdays"].split(",")):
-            self.__run_jobs(row["job_id"])
+            self.__run_jobs(row["job_id" if is_job else "infoprovider_id"], is_job)
             return
 
         # check if type is "daily"
         if row["s_type"] == "daily":
-            self.__run_jobs(row["job_id"])
+            self.__run_jobs(row["job_id" if is_job else "infoprovider_id"], is_job)
             return
 
     def __get_delete_time(self, time):
@@ -63,12 +68,16 @@ class DbScheduler(Scheduler):
     def _check_all(self, now: datetime):
         logger.info(f"Check if something needs to be done at: {now}")
 
-        rows = job.get_job_schedules()
+        jobs = job.get_job_schedules()
+        infoproviders = job.get_infoprovider_schedules()
 
         if int(now.strftime("%M")) == 00:
-            delete_on_time(rows, config_manager.STEPS_BASE_CONFIG["output_path"], "job_name",
+            delete_on_time(jobs, config_manager.STEPS_BASE_CONFIG["output_path"], "job_name",
                            lambda j: j["d_type"] == "on_day_hour",
                            lambda j: self.__get_delete_time(j))
 
-        for row in rows:
-            self.__check(row, now)
+        for j in jobs:
+            self.__check(j, now, True)
+
+        for i in infoproviders:
+            self.__check(i, now, False)
