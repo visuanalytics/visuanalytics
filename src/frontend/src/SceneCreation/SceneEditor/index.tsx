b9fe94a7c66210c766a9447c1bc30f9f1094f064
Janek Berg <janek.berg@mni.thm.de>
2021-05-31 17:08:26 +0200

    added loading data of selected infoprovider and basic list generation for available data in scene editor

--- src/frontend/src/SceneCreation/SceneEditor/index.tsx
+++ src/frontend/src/SceneCreation/SceneEditor/index.tsx
@@ -1,43 +1,73 @@
 import React, {useState} from "react";
+import List from "@material-ui/core/List";
+import {ListItem} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import {DataSource} from "../../CreateInfoProvider";
+import {useStyles} from "../style";
+import {StepFrame} from "../../CreateInfoProvider/StepFrame";
+import {hintContents} from "../../util/hintContents";
 
+interface SceneEditorProps {
+    continueHandler: () => void;
+    backHandler: () => void;
+    infoProvider: Array<DataSource>;
+}
 
+export const SceneEditor: React.FC<SceneEditorProps> = (props) => {
 
-export const SceneEditor = () => {
+    //TODO: only for debugging purposes, remove in production
+    const testDataList = ["data1", "data2", "data3", "data4"]
 
-    //const classes = useStyles();
+    const classes = useStyles();
     // contains the names of the steps to be displayed in the stepper
-    const steps = [
-        "Datenquellen-Typ",
-        "API-Einstellungen",
-        "Datenauswahl",
-        "Formeln",
-        "Historisierung",
-        "Gesamtübersicht",
-        "Diagrammerstellung"
-    ];
-    //the current step of the creation process, numbered by 0 to 6
+    const [dataList, setDataList] = React.useState<Array<string>>([]);
 
-    const [step, setStep] = React.useState(0);
+    //TODO: load dataList from the infoProvider props
 
-
-    const handleContinue = () => {
-        setStep(step + 1);
+    /**
+     * Method to handle the selection of data from the list of available api data.
+     * @param item The item selected by the user
+     */
+    const handleItemSelect = (item: string) => {
+        //TODO: add the selected item to the canvas
+        console.log("user selected item " + item)
     }
 
     /**
-     * Handler for back button that is passed to all sub-components as props.
-     * Decrements the step or returns to the dashboard if the step was 0.
+     * Method that renders the list items to be displayed in the list of all available data.
+     * @param item The item to be displayed
      */
-    const handleBack = () => {
-        //TODO: if step==0, return to dashboard context
-        setStep(step-1)
+    const renderListItem = (item: string) => {
+        return (
+            <ListItem key={item}>
+                <Button onClick={() => handleItemSelect(item)}>
+                    {item}
+                </Button>
+            </ListItem>
+        )
     }
 
-
-
     return (
-        <React.Fragment>
-
-        </React.Fragment>
+        <StepFrame
+            heading={"Szenen-Editor"}
+            hintContent={hintContents.typeSelection}
+        >
+            <Grid container>
+                <Grid item xs={12}>
+                    <Box borderColor="primary.main" border={4} borderRadius={5} className={classes.choiceListFrame}>
+                        <List disablePadding={true}>
+                            {dataList.map((item) => renderListItem(item))}
+                        </List>
+                    </Box>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" onClick={() => setDataList(testDataList)}>
+                        Testdaten
+                    </Button>
+                </Grid>
+            </Grid>
+        </StepFrame>
     );
 }
