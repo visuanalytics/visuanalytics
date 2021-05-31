d3c236e9e643626c9e4e2523fc9ab657f4cab672
Janek Berg <janek.berg@mni.thm.de>
2021-05-31 16:12:45 +0200

    added fetching all infoproviders from backend and radio button selection of the infoprovider to use

--- src/frontend/src/SceneCreation/InfoProviderSelection/index.tsx
+++ src/frontend/src/SceneCreation/InfoProviderSelection/index.tsx
@@ -1,17 +1,84 @@
-import React, {useState} from "react";
+import React from "react";
+import {useStyles} from "../style";
+import {InfoProviderData} from "../index";
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Box from "@material-ui/core/Box";
+import {hintContents} from "../../util/hintContents";
+import {StepFrame} from "../../CreateInfoProvider/StepFrame";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
+interface InfoProviderSelectionProps {
+    continueHandler: () => void;
+    backHandler: () => void;
+    infoProviderList: Array<InfoProviderData>;
+}
 
+export const InfoProviderSelection: React.FC<InfoProviderSelectionProps> = (props) => {
 
-export const InfoProviderSelection = () => {
+    const classes = useStyles();
+    //stores the id currently selected infoprovider - 0 is forbidden by the backend so it can be used as a default value
+    const [selectedId, setSelectedId] = React.useState(0);
 
-    //const classes = useStyles();
-    // contains the names of the steps to be displayed in the stepper
-
-
+    /**
+     * Renders an radio button item in the list of all available Infoproviders.
+     */
+    const renderListItem = (infoProvider: InfoProviderData) => {
+        return (
+            <FormControlLabel value={infoProvider.infoprovider_id} control={
+                <Radio
+                />
+            } label={infoProvider.infoprovider_name} key={infoProvider.infoprovider_id}
+            />
+        )
+    }
 
     return (
-        <React.Fragment>
+        <StepFrame
+            heading={"Infoprovider auswählen"}
+            hintContent={hintContents.typeSelection}
+        >
+            <Grid container>
+                <Grid item xs={12}>
+                    <Typography variant="body1">
+                        Wählen sie den Infoprovider aus, dessen Daten in der Szenenerstellung zur Verfügung stehen sollen.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} className={classes.elementLargeMargin}>
+                    <Box borderColor="primary.main" border={4} borderRadius={5} className={classes.choiceListFrame}>
+                        <FormControl>
+                            <RadioGroup value={selectedId}
+                                        onChange={(e) => setSelectedId(Number(e.target.value))}>
+                                {props.infoProviderList.map((infoProvider) => renderListItem(infoProvider))}
+                            </RadioGroup>
+                        </FormControl>
+                    </Box>
+                </Grid>
+                <Grid item container xs={12} justify="space-between" className={classes.elementLargeMargin}>
+                    <Grid item>
+                        <Button variant="contained"
+                                size="large"
+                                color={"primary"}
+                                onClick={props.backHandler}
+                        >
+                            abbrechen
+                        </Button>
+                    </Grid>
+                    <Grid item className={classes.blockableButtonPrimary}>
+                        <Button disabled={ selectedId === 0 } variant="contained"
+                                size="large"
+                                color={"primary"}
+                                onClick={props.continueHandler}>
+                            weiter
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
 
-        </React.Fragment>
+        </StepFrame>
     );
 }
