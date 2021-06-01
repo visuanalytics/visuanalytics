2e1bcc3923ea54eb085e6fc65f7f772b6f7398cb
Janek Berg <janek.berg@mni.thm.de>
2021-06-01 10:08:54 +0200

    added schedule detection that differentiates between dataSources

--- src/frontend/src/CreateInfoProvider/DiagramCreation/DiagramCreator/HistorizedDiagramCreator/index.tsx
+++ src/frontend/src/CreateInfoProvider/DiagramCreation/DiagramCreator/HistorizedDiagramCreator/index.tsx
@@ -234,14 +234,26 @@ export const HistorizedDiagramCreator: React.FC<HistorizedDiagramCreatorProps> =
      * Uses the schedule object passed as property to check what interval was selected for the corresponding values and returns a displaying string.
      */
     const getIntervalDisplay = () => {
+        //search the dataSource that belongs to the current element
+        let dataSource: DataSource = {} as DataSource;
+        const currentDataSourceName = props.historizedObjects[selectedHistorizedOrdinal].name.split("|")[0];
+        for (let index = 0; index < props.dataSources.length; index++) {
+            const source = props.dataSources[index];
+            if(source.apiName === currentDataSourceName) {
+                dataSource = source;
+                break;
+            }
+        }
+        //extract the schedule from the dataSource
+        const schedule = dataSource.schedule;
         //check for weekly
-        /*if (props.schedule.type === "weekly") {
-            if (props.schedule.weekdays !== undefined && props.schedule.weekdays.length !== 0) {
+        if (schedule.type === "weekly") {
+            if (schedule.weekdays !== undefined && schedule.weekdays.length !== 0) {
                 //check if every day is selected
-                if(props.schedule.weekdays.length === 7) {
+                if(schedule.weekdays.length === 7) {
                     return "24h";
                 }
-                const weekdayNumbers = props.schedule.weekdays.slice();
+                const weekdayNumbers = schedule.weekdays.slice();
                 weekdayNumbers.sort();
                 let weekdayStrings = [getWeekdayString(weekdayNumbers[0])];
                 for(let i = 1; i < weekdayNumbers.length; i++) {
@@ -251,12 +263,12 @@ export const HistorizedDiagramCreator: React.FC<HistorizedDiagramCreatorProps> =
             }
         }
         //check for daily
-        else if (props.schedule.type === "daily") {
+        else if (schedule.type === "daily") {
             return "24h"
         }
         //check for interval
-        else if (props.schedule.type === "interval") {
-            switch (props.schedule.interval) {
+        else if (schedule.type === "interval") {
+            switch (schedule.interval) {
                 case "minute": {
                     return "1m";
                 }
@@ -279,7 +291,7 @@ export const HistorizedDiagramCreator: React.FC<HistorizedDiagramCreatorProps> =
                     return "12h"
                 }
             }
-        }*/
+        }
         return "TO BE DONE"
     }
 
