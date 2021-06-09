4e3cb427acccee9212a6bbf88beaf5f3bb258040
D1amxnd <philipp.helfenritter@gmail.com>
2021-06-09 15:12:04 +0200

    Added css for Editor

--- src/frontend/src/SceneCreation/SceneEditor/TransformerComponent/index.tsx
+++ src/frontend/src/SceneCreation/SceneEditor/TransformerComponent/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Konva from 'konva';
 import {Transformer} from 'react-konva'
 
@@ -14,12 +14,22 @@ export const TransformerComponent: React.FC<TransformerProps> = (props) => {
     let transformer = new Konva.Transformer();
     const selectedShapeName = props.selectedShapeName;
 
+    console.log(transformer);
+    console.log(props.selectedShapeName);
+
     const checkNode = React.useCallback( () => {
+
+      console.log("reached");
+      if (transformer !== null){  
+
         transformer.detach();
         const stage = transformer.getStage();
-    
-        var selectedNode = stage!.findOne("." + selectedShapeName);
-    
+        if (stage !== undefined){
+          console.log(stage);
+          var selectedNode = stage!.findOne("." + selectedShapeName);
+
+          console.log(selectedNode)
+
         if (selectedNode !== undefined && selectedNode){
           if (selectedNode ===  transformer.nodes([selectedNode])) {
             return;
@@ -30,12 +40,18 @@ export const TransformerComponent: React.FC<TransformerProps> = (props) => {
         }
     
         transformer.getLayer()!.batchDraw();
+        }
       }
+    }
     ,[]);
+
+    useEffect(() => {
+      checkNode();
+    }, [props.selectedShapeName]);
 
     return (
         <Transformer 
-            ref={(node : Konva.Transformer) => {transformer = node}}
+            ref={node=> {transformer = node!}}
         />
     );    
 }
