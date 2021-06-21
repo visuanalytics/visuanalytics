8c5bb4be07847c4f0fdd65b5c737c44a2256dede
Janek Berg <janek.berg@mni.thm.de>
2021-06-21 11:21:37 +0200

    added basic component for infoproviderlist and fetching from backend

--- src/frontend/src/VideoCreation/style.tsx
+++ src/frontend/src/VideoCreation/style.tsx
@@ -11,6 +11,12 @@ export const useStyles = makeStyles((theme: Theme) =>
                 color: theme.palette.error.dark,
             }
         },
+        blockableButtonPrimary: {
+            '& .Mui-disabled': {
+                backgroundColor: theme.palette.primary.main,
+                opacity: "75%"
+            }
+        },
         blockableButtonSecondary: {
             margin: "auto auto",
             marginLeft: "30px",
@@ -36,6 +42,13 @@ export const useStyles = makeStyles((theme: Theme) =>
         alignedButton: {
             margin: "auto auto",
         },
-
+        listFrame: {
+            width: "100%",
+            height: 400,
+            overflow: 'auto',
+            overflowX: "hidden",
+            marginTop: "20px",
+            marginBottom: "10px"
+        },
     }),
 );
