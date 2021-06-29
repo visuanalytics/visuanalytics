d66a28c12915846a080138fa532b1ca5a2c9e94d
Daniel Spengler <daniel.spengler@mni.thm.de>
2021-06-29 14:56:02 +0200

    Added doc strings for EditTextDialog

--- src/frontend/src/VideoCreation/VideoEditor/EditTextDialog/index.tsx
+++ src/frontend/src/VideoCreation/VideoEditor/EditTextDialog/index.tsx
@@ -24,6 +24,9 @@ import { Schedule } from "../../../CreateInfoProvider/types";
 import {ScheduleTypeTable} from "../../../CreateInfoProvider/SettingsOverview/ScheduleTypeTable";
 import Box from "@material-ui/core/Box";
 
+/**
+ * This interface contains the values the component gets, passed in by props
+ */
 interface EditTextDialogProps {
     sceneName: string;
     openEditTextDialog: boolean;
@@ -33,38 +36,65 @@ interface EditTextDialogProps {
     minimalInfoproviders: Array<MinimalInfoProvider>;
 }
 
+/**
+ * Component that renders the information and input fields for adding TTS to a scene
+ * This component could have been added to `SceneCard` directly, but was out sourced, for better readability.
+ * @param props The props, the component gets passed in
+ */
 export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
     const classes = useStyles();
 
+    // The state, which holds the value for a pause that should be added. Please not, that this state is not used for modifying existing pauses
     const [newPause, setNewPause] = React.useState<number | undefined>(0);
 
+    // This state holds all information for the audio of the scene. Audio canbe text or pause at the moment. If non spoken text is provided, an audio element with no text is generated for the state
     const [audioElements, setAudioElements] = React.useState<Array<AudioElement>>(props.spokenText.length === 0 ? [{
         type: "text",
         text: ""
     }] : props.spokenText);
 
+    // This state holds an array with values for all given infoproviders if their data should be extended or not. The default value should be no extension
     const [showInfoproviderData, setShowInfoproviderData] = React.useState<Array<boolean>>(new Array(props.minimalInfoproviders.length).fill(false));
 
+    // This state holds the last index from audioElements, which was edited and was of type text. This will be needed for inserting Infoprovider data
     const [lastEditedTTSTextIndex, setLastEditedTTSTextIndex] = React.useState<number>(0);
 
+    // State, that defines, if the dialog for setting intervals for historized data should be shown or not
     const [showAddHistorizedDialog, setShowHistorizedDialog] = React.useState(false);
 
+    // Holds the selected value of the clicked historized element, needed for the interval selection and inserting process of historized data
     const [selectedHistorizedElement, setSelectedHistorizedElement] = React.useState("");
 
+    // Holds the schedule that applies for the selected historized element
     const [selectedSchedule, setSelectedSchedule] = React.useState<Schedule>({type: "", time: "", interval: "", weekdays: []});
 
+    // This state holds the interval which a user wants to use for the historized element
     const [selectedInterval, setSelectedInterval] = React.useState<number | undefined>(0);
 
+    /**
+     * Changes the value of the last edited text element in the audioElements.
+     * @param selectedIndex The index that belongs to the last clicked TTS-textfield
+     */
     const handleClickOnTTSTextField = (selectedIndex: number) => {
         setLastEditedTTSTextIndex(selectedIndex);
     }
 
+    /**
+     * Inserts the data from an selected element or from custom data.
+     * For both categories the insertion process is equal, so there is no need of two methods.
+     * The item is always inserted to the textfield, which was clicked last by the mouse.
+     * @param item The item name that should be added to the textfield. This sthould be the full path, beginning with the infoprovider name
+     */
     const insertSelectedOrCustomData = (item: string) => {
         const arrCopy = audioElements.slice();
         arrCopy[lastEditedTTSTextIndex].text = arrCopy[lastEditedTTSTextIndex].text + "{:" + item + ":} ";
         setAudioElements(arrCopy);
     }
 
+    /**
+     * Inserts historized data in the last clicked TTS-textfield.
+     * After finishing this, the used states are reset to their default values, as declared above.
+     */
     const insertHistorizedData = () => {
         if(selectedInterval !== undefined) {
             const arrCopy = audioElements.slice();
@@ -75,12 +105,21 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         }
     }
 
+    /**
+     * Toggles, if the data of a clicked infoprovider should be visible or not.
+     * @param index
+     */
     const toggleInfoproviderData = (index: number) => {
         const arrCopy = showInfoproviderData.slice();
         arrCopy[index] = !arrCopy[index];
         setShowInfoproviderData(arrCopy);
     }
 
+    /**
+     * Adds a new textfield to the edit dialog.
+     * Before this textfield, the selected pause from the user is inserted.
+     * After this operation the newPause state will be reseted to its default value
+     */
     const addNewText = () => {
         const arrCopy = audioElements.slice();
         arrCopy.push({
@@ -94,6 +133,12 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         setNewPause(0);
     }
 
+    /**
+     * Deletes a block of pause and text
+     * In almost all cases the textfield left of the button will be deleted and the following pause
+     * Just in the case that the textfield is the last element, the pause before this field will be deleted.
+     * @param index The index of the textfield for which the button was clicked
+     */
     const deleteText = (index: number) => {
         const arrCopy = audioElements.slice();
         if(index >= audioElements.length - 2) arrCopy.splice(index - 1, 2);
@@ -101,6 +146,12 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         setAudioElements(arrCopy);
     }
 
+    /**
+     * Everytime a user changes an existing text element or pause this method will be called to update the corresponding fields and states
+     * The ID of the target is used to identifiy the index in the audioElements-array.
+     * If the type of the element, which should be added is pause, the value of the event will be converted to a number, othervice the string will be used without further conversions
+     * @param event The event sent from the element
+     */
     const changeElement = (event: React.ChangeEvent<HTMLInputElement>) => {
         const editedElement = Number(event.target.id);
         const arrCopy = audioElements.slice();
@@ -109,11 +160,18 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         setAudioElements(arrCopy);
     }
 
+    /**
+     * Writes the audioElements to the higher state and closes the dialog for adding and editing text and audio
+     */
     const saveNewAudio = () => {
         props.setSpokenText(audioElements.slice());
         props.setOpenEditTextDialog(false);
     }
 
+    /**
+     * Checks if all textfields are set with an valid input
+     * "valid" means that no textfield is empty and that the pause is not less than 0ms
+     */
     const isInvalidCombination = () => {
         for(let i = 0; i < audioElements.length; i++) {
             if(audioElements[i].type === "text" && audioElements[i].text === "") return true;
@@ -122,6 +180,10 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         return false;
     }
 
+    /**
+     * If the user selected an historized element and decides to not insert it, this method will be called to clear all states
+     * After the clean up the dialog for historized data interval selection will be closed as well.
+     */
     const cancelHistorizedDataAdding = () => {
         setSelectedSchedule({type: "", time: "", interval: "", weekdays: []});
         setSelectedHistorizedElement("");
@@ -129,6 +191,13 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         setShowHistorizedDialog(false);
     }
 
+    /**
+     * This method renders the input fields for the audio element which is passed in
+     * The given index of the element will be used as the ID for the input fields
+     * Dependend on the type of the current element, different fields will be rendered. This way it is not required to have two methods.
+     * @param id This is the index of the element from the audioElements-array.
+     * @param audioElement The audio element which should be rendered
+     */
     const renderEditElement = (id: number, audioElement: AudioElement) => {
         if(audioElement.type === "text" && audioElement.text !== undefined) {
             return (
@@ -152,6 +221,12 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         }
     }
 
+    /**
+     * Method, which is called, when the user clicks on a historized element
+     * This is needed to prepare all states and then opening the dialog for the interval selection
+     * @param item The string (item name) which belonges to the historized element
+     * @param schedule The corresponding schedule object for the selected element
+     */
     const handleClickOnHistorizedData = (item: string, schedule: Schedule) => {
         setSelectedHistorizedElement(item);
         setSelectedSchedule(schedule);
@@ -159,6 +234,11 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
     }
 
     //TODO: possibly find better solution than abusing span
+    /**
+     * Renders clickable buttons for the historized data of an infoprovider, so they can be inserted into textfields
+     * @param item The item name
+     * @param schedule The corresponding schedule for the rendered element, this is needed for passing to the next dialog if clicked
+     */
     const renderHistorizedData = (item: string, schedule: Schedule) => {
         return (
             <ListItem key={item}>
@@ -170,6 +250,10 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
     }
 
     //TODO: possibly find better solution than abusing span
+    /**
+     * This method renders clickable buttons for inserting selected and custom data from an infoprovider into a textfield.
+     * @param item The name of the item that should be rendered
+     */
     const renderSelectedDataAndCustomData = (item: string) => {
         return (
             <ListItem key={item}>
@@ -180,6 +264,11 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         );
     }
 
+    /**
+     * Renders all information for a single data source of an infoprovider. This information includes, selected, custom and historized data
+     * @param dataSource The data source that should be rendered
+     * @param infoProviderName The infoprovider name that belonges to the data source. This is needed for constructing the whole path of an item
+     */
     const renderDataSource = (dataSource: MinimalDataSource, infoProviderName: string) => {
         return (
             <Grid item container xs={12}>
@@ -217,6 +306,12 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         );
     }
 
+    /**
+     * Renders all information for one infoprovider
+     * @param dataSources The data sources the infoprovider holds
+     * @param infoproviderName The name of the infoprovider that should be rendered
+     * @param infoproviderIndex The index of the infoprovider in the array. This is needed for toggeling the visibility of its data
+     */
     const renderInfoproviderData = (dataSources: Array<MinimalDataSource>, infoproviderName: string, infoproviderIndex: number) => {
         return (
             <Grid key={infoproviderName} item container xs={12}>
@@ -251,6 +346,10 @@ export const EditTextDialog: React.FC<EditTextDialogProps> = (props) => {
         );
     }
 
+    /**
+     * This renders the representation of the component
+     * The rendering also includes the second dialog which is used for selecting intervals for historized elements
+     */
     return (
         <React.Fragment>
             <Dialog aria-labelledby="EditTextDialog-Title" maxWidth="md" open={props.openEditTextDialog}>
