d099270200ad5e1c882489214a1ca49079bb430d
Janek Berg <janek.berg@mni.thm.de>
2021-07-24 13:14:42 +0200

    added missing helpermethods

---
+++ src/frontend/src/SceneCreation/helpermethods.tsx
@@ -0,0 +1,56 @@
+import {Schedule} from "../CreateInfoProvider/types";
+import {getWeekdayString} from "../CreateInfoProvider/helpermethods";
+
+/**
+ * Calculates a string that displays the interval scheme of a given schedule
+ */
+export const getIntervalDisplay = (schedule: Schedule) => {
+    //check for weekly
+    if (schedule.type === "weekly") {
+        if (schedule.weekdays !== undefined && schedule.weekdays.length !== 0) {
+            //check if every day is selected
+            if(schedule.weekdays.length === 7) {
+                return "24h";
+            }
+            const weekdayNumbers = schedule.weekdays.slice();
+            weekdayNumbers.sort();
+            let weekdayStrings = [getWeekdayString(weekdayNumbers[0])];
+            for(let i = 1; i < weekdayNumbers.length; i++) {
+                weekdayStrings.push(getWeekdayString(weekdayNumbers[i]));
+            }
+            return "Wochentage ("  + weekdayStrings.join(", ") + ")";
+        }
+    }
+    //check for daily
+    else if (schedule.type === "daily") {
+        return "24h"
+    }
+    //check for interval
+    else if (schedule.type === "interval") {
+        switch (schedule.interval) {
+            case "minute": {
+                return "1m";
+            }
+            case "quarter": {
+                return "15m";
+            }
+            case "half": {
+                return "30m";
+            }
+            case "threequarter": {
+                return "45m";
+            }
+            case "hour": {
+                return "1h";
+            }
+            case "quartday": {
+                return "6h";
+            }
+            case "halfday": {
+                return "12h"
+            }
+        }
+    }
+    //TODO: ??? what should happen here?
+    return "TO BE DONE"
+}
